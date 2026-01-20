3c720f9928778d31871b810d128f1577cf8535fa
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-20 16:10:49 +0900

    chore: main.tsx 타입 단언 제거

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,8 +5,12 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
