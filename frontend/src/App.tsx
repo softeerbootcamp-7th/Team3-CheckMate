c7fff17fd29b2ccb08ecaf5d732f1f4ce4245a68
minseon <mskwon0601@gmail.com>
2026-01-22 15:00:06 +0900

    chore: public 폴더 바로 밑에 있던 font, .svg 파일 public/assets로 이동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 
 import reactLogo from './assets/react.svg';
 
-import viteLogo from '/vite.svg';
+import viteLogo from '/assets/vite.svg';
 
 function App() {
   const [count, setCount] = useState(0);
