becee35b8f1167685fd8aa6ed6576bf2392436f2
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-26 01:02:55 +0900

    feat: 캘린더 날짜 선택 cell 컴포넌트 추가

---
+++ frontend/src/components/shared/calendar/Calendar.date-cell.tsx
@@ -0,0 +1,29 @@
+import { cn } from '@/utils/shared';
+
+import { Button } from '../shadcn-ui';
+
+interface CalendarDateCellProps {
+  date: number;
+  onClick?: () => void;
+  className?: string;
+}
+
+export const CalendarDateCell = ({
+  date,
+  onClick,
+  className,
+}: CalendarDateCellProps) => {
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      className={cn(
+        'body-small-semibold flex size-10.5 items-center justify-center rounded-none',
+        className,
+      )}
+      onClick={onClick}
+    >
+      {date}
+    </Button>
+  );
+};
