2f2353737f3cef9136718b62150c2d49de25cf86
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-27 02:20:49 +0900

    fix: 시작, 종료날짜가 둘 다 설정되어 있을 때, 종료날짜를 시작 날짜로 선택하지 못하는 문제 해결

--- frontend/src/hooks/shared/calendar/useDateCalendar.ts
+++ frontend/src/hooks/shared/calendar/useDateCalendar.ts
@@ -17,8 +17,8 @@ export const useDateCalendar = ({
       return;
     }
 
-    // 시작 날짜가 있고 종료 날짜가 없을 때, 선택한 날짜가 시작 날짜보다 크면 종료 날짜로 설정
-    // 작으면 시작 날짜를 종료 날짜를 설정하고 시작 날짜를 선택한 날짜로 설정
+    // 시작 날짜가 있고 종료 날짜가 없을 때, 선택한 날짜가 시작 날짜보다 이후이면 종료 날짜로 설정
+    // 이전이면 시작 날짜를 종료 날짜를 설정하고 시작 날짜를 선택한 날짜로 설정
     if (selectedStartDate && !selectedEndDate) {
       const currentSelectedStartDate = new Date(selectedStartDate);
       if (currentDate.getTime() > selectedStartDate.getTime()) {
@@ -35,16 +35,22 @@ export const useDateCalendar = ({
     if (selectedStartDate && selectedEndDate) {
       const currentSelectedStartDate = new Date(selectedStartDate);
       const currentSelectedEndDate = new Date(selectedEndDate);
-      // 선택한 날짜가 종료 날짜보다 크거나 같으면 종료 날짜로 설정
+      // 선택한 날짜가 종료 날짜보다 이후이면 종료 날짜로 설정
       if (currentDate.getTime() > currentSelectedEndDate.getTime()) {
         setSelectedEndDate(currentDate);
         return;
       }
 
-      // 선택한 날짜가 시작 날짜보다 작으면 시작 날짜를 설정하고 종료 날짜를 설정
-      if (currentDate.getTime() <= currentSelectedStartDate.getTime()) {
+      // 선택한 날짜가 시작 날짜보다 이전이면 시작 날짜를 설정하고 종료 날짜를 설정
+      if (currentDate.getTime() < currentSelectedStartDate.getTime()) {
         setSelectedStartDate(currentDate);
-        setSelectedEndDate(currentSelectedStartDate);
+        return;
+      }
+
+      // 선택한 날짜가 시작 날짜와 동일하면 종료 날짜를 설정
+      if (currentDate.getTime() === currentSelectedStartDate.getTime()) {
+        setSelectedEndDate(currentDate);
+        return;
       }
 
       // 선택한 날짜가 시작 날짜와 종료 날짜 사이에 있으면 종료 날짜를 설정
