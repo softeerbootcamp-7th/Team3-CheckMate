22d6553f6f959cf8142b31e6a68274449c4d6256
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-26 12:01:29 +0900

    feat: date range picker side 컴포넌트 memo 적용

--- frontend/src/components/shared/date-range-picker/DateRangePicker.side.tsx
+++ frontend/src/components/shared/date-range-picker/DateRangePicker.side.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 import { Info } from 'lucide-react';
 
 import { type DateRangePickerType } from '@/constants/shared';
@@ -19,69 +21,73 @@ interface DateRangePickerSideProps {
   dateRangePickerType: DateRangePickerType;
 }
 
-export const DateRangePickerSide = ({
-  selectedStartDate,
-  selectedEndDate,
-  setSelectedStartDate,
-  setSelectedEndDate,
-  handleCancel,
-  handleSave,
-  dateRangePickerType,
-}: DateRangePickerSideProps) => {
-  const {
-    alertText,
-    isAllDateRangeSelected,
-    isValidDateRange,
-    formattedStartDate,
-    formattedEndDate,
-    handleResetStartDate,
-    handleResetEndDate,
-  } = useDateRangePickerSide({
+export const DateRangePickerSide = memo(
+  ({
     selectedStartDate,
     selectedEndDate,
     setSelectedStartDate,
     setSelectedEndDate,
+    handleCancel,
+    handleSave,
     dateRangePickerType,
-  });
+  }: DateRangePickerSideProps) => {
+    const {
+      alertText,
+      isAllDateRangeSelected,
+      isValidDateRange,
+      formattedStartDate,
+      formattedEndDate,
+      handleResetStartDate,
+      handleResetEndDate,
+    } = useDateRangePickerSide({
+      selectedStartDate,
+      selectedEndDate,
+      setSelectedStartDate,
+      setSelectedEndDate,
+      dateRangePickerType,
+    });
 
-  return (
-    <div className="flex w-58.75 flex-col justify-between pt-250">
-      <div className="flex flex-col gap-600">
-        <div className="body-medium-bold">기간 선택</div>
-        <div className="flex flex-col gap-350">
-          <div className="flex items-center gap-350">
-            <DateRangePickerStartEndIndicator />
-            <div className="flex grow flex-col gap-350">
-              <DateRangePickerSelectedDate
-                selectedDate={formattedStartDate}
-                resetDate={handleResetStartDate}
-                label="시작"
-              />
-              <DateRangePickerSelectedDate
-                selectedDate={formattedEndDate}
-                resetDate={handleResetEndDate}
-                label="종료"
-              />
+    return (
+      <div className="flex w-58.75 flex-col justify-between pt-250">
+        <div className="flex flex-col gap-600">
+          <div className="body-medium-bold">기간 선택</div>
+          <div className="flex flex-col gap-350">
+            <div className="flex items-center gap-350">
+              <DateRangePickerStartEndIndicator />
+              <div className="flex grow flex-col gap-350">
+                <DateRangePickerSelectedDate
+                  selectedDate={formattedStartDate}
+                  resetDate={handleResetStartDate}
+                  label="시작"
+                />
+                <DateRangePickerSelectedDate
+                  selectedDate={formattedEndDate}
+                  resetDate={handleResetEndDate}
+                  label="종료"
+                />
+              </div>
+            </div>
+            <div
+              className={cn(
+                'caption-large flex items-center gap-50',
+                isValidDateRange ? 'text-grey-700' : 'text-others-negative',
+              )}
+            >
+              <Info className="size-3.5" />
+              <span>{alertText}</span>
             </div>
           </div>
-          <div
-            className={cn(
-              'caption-large flex items-center gap-50',
-              isValidDateRange ? 'text-grey-700' : 'text-others-negative',
-            )}
-          >
-            <Info className="size-3.5" />
-            <span>{alertText}</span>
-          </div>
+        </div>
+        <div className="flex items-center justify-end gap-200">
+          <DateRangePickerCancelButton handleCancel={handleCancel} />
+          <DateRangePickerSaveButton
+            handleSave={handleSave}
+            disabled={!isAllDateRangeSelected || !isValidDateRange}
+          />
         </div>
       </div>
-      <div className="flex items-center justify-end gap-200">
-        <DateRangePickerCancelButton handleCancel={handleCancel} />
-        <DateRangePickerSaveButton
-          handleSave={handleSave}
-          disabled={!isAllDateRangeSelected || !isValidDateRange}
-        />
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
+
+DateRangePickerSide.displayName = 'DateRangePickerSide';
