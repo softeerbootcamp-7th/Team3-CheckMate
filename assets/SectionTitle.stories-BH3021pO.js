1050bb61f9762cae30d1083e0b1aef30aa73441b
lwjmcn <lwjmcn@users.noreply.github.com>
2026-01-27 07:29:54 +0000

    deploy: 1d03d45a3a675d0bb8c9bfe8f9c773615744feb6

---
+++ assets/SectionTitle.stories-BH3021pO.js
@@ -0,0 +1,6 @@
+import{S as e}from"./SectionTitle-BspKs-hs.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-BvUYDXD7.js";import"./preload-helper-PPVm8Dsz.js";const n={title:"components/shared/section-title/SectionTitle",component:e,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{title:{control:"text"},description:{control:"text"}}},t={args:{title:"매출 현황",description:"실제 매출과 주문 상황을 한눈에 확인해요."}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: '매출 현황',
+    description: '실제 매출과 주문 상황을 한눈에 확인해요.'
+  }
+}`,...t.parameters?.docs?.source}}};const i=["Default"];export{t as Default,i as __namedExportsOrder,n as default};
