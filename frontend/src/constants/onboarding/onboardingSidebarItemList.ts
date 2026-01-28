3436b3b46b69a297c847dd43b40a2ad3baa9740a
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-28 18:12:45 +0900

    feat: 온보딩 사이드바 item 추가

---
+++ frontend/src/constants/onboarding/onboardingSidebarItemList.ts
@@ -0,0 +1,14 @@
+export const ONBOARDING_SIDEBAR_ITEM_LIST = [
+  {
+    step: 1,
+    label: '사업자 확인',
+  },
+  {
+    step: 2,
+    label: '매장 정보 입력',
+  },
+  {
+    step: 3,
+    label: '영업시간 입력',
+  },
+] as const;
