07b08d97297d1a5ea2821491940eef9a1d618e66
jho7535 <jho7535@naver.com>
2026-01-29 23:46:14 +0900

    feat: 멤버와 매장 관계 설정 및 조회 기능 추가
    - Member 엔티티에 Store 엔티티와의 관계 설정 추가 (OneToOne)
    - MemberRepository에 ID와 함께 Store 엔티티를 가져오는 쿼리 추가 (fetch join)

--- backend/src/main/java/com/checkmate/backend/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/checkmate/backend/domain/member/repository/MemberRepository.java
@@ -3,7 +3,12 @@ package com.checkmate.backend.domain.member.repository;
 import com.checkmate.backend.domain.member.entity.Member;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    @Query("select m from Member m join fetch m.store where m.id = :id")
+    Optional<Member> findByIdWithStore(@Param("id") Long id);
 }
