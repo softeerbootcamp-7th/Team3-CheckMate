4249ffdcfba522907b0b52ec8e96397569403786
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-30 02:04:40 +0900

    feat: 매장 영업 시간 input grid 제목 행 컴포넌트 추가

---
+++ frontend/src/components/onboarding/store-register/store-business-hours-row/StoreBusinessHoursHeaderRow.tsx
@@ -0,0 +1,15 @@
+export const StoreBusinessHoursHeaderRow = () => {
+  return (
+    <>
+      <span className="body-medium-semibold text-grey-600 col-start-2 text-center">
+        영업시작
+      </span>
+      <span className="body-medium-semibold text-grey-600 col-start-4 text-center">
+        영업마감
+      </span>
+      <span className="body-medium-semibold text-grey-600 col-start-5 text-center">
+        휴무
+      </span>
+    </>
+  );
+};
