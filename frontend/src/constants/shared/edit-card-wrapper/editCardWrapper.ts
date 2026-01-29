cfd1274542482f75697def4b02c7cba01cc69fc3
minseon <mskwon0601@gmail.com>
2026-01-29 18:30:10 +0900

    chore: 상수들 constants/shared/edit-card-wrapper 에 분리

---
+++ frontend/src/constants/shared/edit-card-wrapper/editCardWrapper.ts
@@ -0,0 +1,6 @@
+export const CHANGE_SCALE = 0.65; // 65% 축소
+export const PADDING_SIZE = 12; // 패딩 박스 내 여백 사이즈(12px)
+export const HEADER_HEIGHT = 26; // 헤더 높이(위에 있는 버튼들 높이 : 26px)
+export const HEADER_MAIN_GAP = 16; // 헤더와 내용 사이 간격(16px)
+export const MIN_WIDTH = 220; // 최소 너비 220px
+export const MIN_HEIGHT = 147; // 최소 높이 147px
---
+++ frontend/src/constants/shared/edit-card-wrapper/index.ts
@@ -0,0 +1,8 @@
+export {
+  CHANGE_SCALE,
+  PADDING_SIZE,
+  HEADER_HEIGHT,
+  HEADER_MAIN_GAP,
+  MIN_WIDTH,
+  MIN_HEIGHT,
+} from './editCardWrapper';
