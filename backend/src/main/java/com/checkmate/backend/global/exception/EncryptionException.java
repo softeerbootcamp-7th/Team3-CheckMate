543c89b4cf81751e54d8d6eec642128e12f3bd11
jho7535 <jho7535@naver.com>
2026-01-29 10:15:41 +0900

    feat: 암호화 예외 클래스 추가
    - EncryptionException 클래스를 추가하여 암호화 관련 예외를 처리
    - 예외 발생 시 메시지와 원인을 함께 전달

---
+++ backend/src/main/java/com/checkmate/backend/global/exception/EncryptionException.java
@@ -0,0 +1,7 @@
+package com.checkmate.backend.global.exception;
+
+public class EncryptionException extends RuntimeException {
+    public EncryptionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
