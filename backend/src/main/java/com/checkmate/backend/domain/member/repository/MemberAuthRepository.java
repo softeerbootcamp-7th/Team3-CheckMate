e6a098ebb3fc2888876ba2bb79dee83084861bc1
Hanul Jeong <jho7535@naver.com>
2026-01-29 17:01:24 +0900

    [BE] 회원가입 로직 구현 (#151)

--- backend/src/main/java/com/checkmate/backend/domain/member/repository/MemberAuthRepository.java
+++ backend/src/main/java/com/checkmate/backend/domain/member/repository/MemberAuthRepository.java
@@ -1,9 +1,10 @@
 package com.checkmate.backend.domain.member.repository;
 
 import com.checkmate.backend.domain.member.entity.Member;
+import com.checkmate.backend.domain.member.entity.MemberAuth;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
-  Optional<Member> findByEmail(String email);
+public interface MemberAuthRepository extends JpaRepository<MemberAuth, Long> {
+  Optional<MemberAuth> findByMember(Member member);
 }
