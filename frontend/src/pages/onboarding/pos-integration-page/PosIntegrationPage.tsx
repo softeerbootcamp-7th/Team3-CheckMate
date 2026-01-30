f36aaf69f48b929fba0cee897d0630f5d66a66b3
lee0jae330 <xingxing2001@cau.ac.kr>
2026-01-31 01:54:35 +0900

    chore: 포스 연동 페이지 기본 section을 guide section으로 수정

--- frontend/src/pages/onboarding/pos-integration-page/PosIntegrationPage.tsx
+++ frontend/src/pages/onboarding/pos-integration-page/PosIntegrationPage.tsx
@@ -2,6 +2,7 @@ import { PosIntegrationGuideSection } from '@/components/onboarding/pos-integrat
 import { OnboardingSidebar } from '@/components/onboarding/shared';
 import { OnboardingLayout } from '@/components/shared';
 
+// TODO: POS 연동 상태 따른 section 조건부 렌더링 필요
 export const PosIntegrationPage = () => {
   return (
     <OnboardingLayout>
