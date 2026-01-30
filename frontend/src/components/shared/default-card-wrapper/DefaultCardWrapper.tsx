4ae6994e01d86e33ac64dcf4488919ec165a4f73
minseon <mskwon0601@gmail.com>
2026-01-30 09:56:57 +0900

    feat: 대시보드, 상세분석에 사용되는 카드로 만들어주는 wrapper 구현

---
+++ frontend/src/components/shared/default-card-wrapper/DefaultCardWrapper.tsx
@@ -0,0 +1,46 @@
+import type { ReactNode } from 'react';
+
+import { cn } from '@/utils/shared';
+
+interface DefaultCardWrapperProps {
+  children: ReactNode;
+  title?: string;
+  titleIcon?: ReactNode;
+  onClickTitleIcon?: () => void;
+  className?: string;
+  width?: number;
+  height?: number;
+}
+
+export const DefaultCardWrapper = ({
+  children,
+  title,
+  titleIcon,
+  onClickTitleIcon,
+  className,
+  width,
+  height,
+}: DefaultCardWrapperProps) => {
+  return (
+    <article
+      style={{ width, height }}
+      className={cn(
+        'bg-special-card-bg rounded-400 flex flex-col justify-between p-5',
+        className,
+      )}
+    >
+      <div className="relative flex items-center text-gray-700">
+        {title && <h3 className="body-medium-semibold">{title}</h3>}
+        {titleIcon && (
+          <button
+            className="size-4 [&>svg]:size-full"
+            onClick={onClickTitleIcon}
+          >
+            {titleIcon}
+          </button>
+        )}
+      </div>
+      {children}
+    </article>
+  );
+};
