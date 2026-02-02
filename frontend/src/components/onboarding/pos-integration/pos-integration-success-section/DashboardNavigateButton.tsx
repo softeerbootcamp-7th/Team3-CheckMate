74de33171ab56e926c109d9611d5db60dec589be
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-02 23:28:15 +0900

    feat: 대시보드 바로가기 버튼 자식요소로 Link 태그 추가 및 asChild 활용

--- frontend/src/components/onboarding/pos-integration/pos-integration-success-section/DashboardNavigateButton.tsx
+++ frontend/src/components/onboarding/pos-integration/pos-integration-success-section/DashboardNavigateButton.tsx
@@ -1,21 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { Button } from '@/components/shared/shadcn-ui';
 
 export const DashboardNavigateButton = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/dashboard', {
-      replace: true,
-    });
-  };
   return (
     <Button
       className="rounded-150 bg-brand-main text-grey-50 title-small-semibold! flex h-14 w-55 items-center justify-center"
       type="button"
-      onClick={handleClick}
+      asChild
     >
-      대시보드 바로가기
+      <Link to="/dashboard" replace>
+        대시보드 바로가기
+      </Link>
     </Button>
   );
 };
