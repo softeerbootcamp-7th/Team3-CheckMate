ac63ec3aaf5d2cf7c6b8b143d50300d359215561
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-03 00:14:52 +0900

    feat: 사업자 정보 입력 관련 훅 분리 및 적용

--- frontend/src/components/onboarding/store-register/business-registration-number-input-section/BusinessRegistrationNumberInputSection.tsx
+++ frontend/src/components/onboarding/store-register/business-registration-number-input-section/BusinessRegistrationNumberInputSection.tsx
@@ -1,69 +1,26 @@
-import { useRef } from 'react';
-import { useController, useFormContext } from 'react-hook-form';
-
-import { useMutation } from '@tanstack/react-query';
-
 import { Input } from '@/components/shared';
-import { Button } from '@/components/shared/shadcn-ui';
-import { STORE_REGISTER_FORM_FIELD } from '@/constants/onboarding/store-register';
-import { postBusinessRegistrationNumber } from '@/services/onboarding/store-register';
-import type { StoreRegisterForm } from '@/types/onboarding/store-register';
-import { validateBusinessRegistrationNumber } from '@/utils/onboarding/store-register';
+import { useBusinessRegistration } from '@/hooks/onboarding/store-register';
 import { cn } from '@/utils/shared';
 
+import { NextStepButton } from '../next-step-button';
 import { StoreRegisterFormTitle } from '../store-register-form-title';
 
+import { BusinessRegistrationNumberVerifyButton } from './BusinessRegistrationNumberVerifyButton';
+
 export const BusinessRegistrationNumberInputSection = () => {
-  const { control, setValue, watch } = useFormContext<StoreRegisterForm>();
   const {
-    field: { ref, onChange, onBlur, value },
-    fieldState: { error, isTouched, invalid },
-  } = useController({
-    name: STORE_REGISTER_FORM_FIELD.BUSINESS_REGISTRATION_NUMBER,
-    control,
-    rules: {
-      validate: validateBusinessRegistrationNumber,
-    },
-    defaultValue: '',
-  });
-
-  const businessAuthToken = watch(
-    STORE_REGISTER_FORM_FIELD.BUSINESS_AUTH_TOKEN,
-  );
-
-  const { mutate } = useMutation({
-    mutationFn: postBusinessRegistrationNumber,
-    onSuccess: ({ businessAuthToken }) => {
-      setValue(
-        STORE_REGISTER_FORM_FIELD.BUSINESS_AUTH_TOKEN,
-        businessAuthToken,
-      );
-    },
-    onError: (error) => {
-      console.error(error);
-    },
-  });
-
-  const verifyButtonRef = useRef<HTMLButtonElement>(null);
-
-  const handleVerifyBusinessRegistrationNumber = () => {
-    mutate({
-      businessRegistrationNumber: value,
-    });
-  };
-
-  const handlePreventEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      verifyButtonRef.current?.click();
-    }
-  };
-
-  // error가 있고 한번 이상 blur된 경우
-  const isError = !!error && isTouched;
-
-  // error가 있거나 값이 없는 경우
-  const isDisabled = invalid || !value;
+    ref,
+    onBlur,
+    value,
+    error,
+    businessAuthToken,
+    verifyButtonRef,
+    isError,
+    isSuccess,
+    isDisabled,
+    handlePreventEnter,
+    handleBusinessRegistrationNumberChange,
+  } = useBusinessRegistration();
 
   return (
     <>
@@ -71,38 +28,31 @@ export const BusinessRegistrationNumberInputSection = () => {
         title={`안녕하세요 사장님! \n 매장 등록을 시작할게요`}
       />
       <div
-        className={cn('flex w-full flex-col', isError ? 'gap-0.5' : 'gap-8')}
+        className={cn(
+          'flex w-full flex-col',
+          isError || isSuccess ? 'gap-0.5' : 'gap-8',
+        )}
       >
         <Input
           label="매장 사업자등록번호"
           placeholder="-없이 숫자만 입력"
           ref={ref}
-          onChange={onChange}
+          onChange={handleBusinessRegistrationNumberChange}
           onBlur={onBlur}
           value={value}
-          isError={isError && isTouched}
+          isError={isError}
           errorMessage={error?.message}
           onKeyDown={handlePreventEnter}
+          isSuccess={!!businessAuthToken}
+          successMessage="사업자 인증이 완료되었어요."
         />
         {businessAuthToken ? (
-          <div>
-            <p>사업자 인증 성공</p>
-          </div>
+          <NextStepButton />
         ) : (
-          <Button
-            type="button"
-            className={cn(
-              'rounded-150 bg-grey-100 title-small-semibold! text-grey-500 flex h-12 w-20 items-center justify-center self-end px-3 py-2',
-              isDisabled
-                ? 'bg-grey-100 text-grey-500'
-                : 'text-grey-50 bg-brand-main',
-            )}
-            onClick={handleVerifyBusinessRegistrationNumber}
+          <BusinessRegistrationNumberVerifyButton
             disabled={isDisabled}
             ref={verifyButtonRef}
-          >
-            조회
-          </Button>
+          />
         )}
       </div>
     </>
