b0065b1776cca6a8472df2b163cf911adb321124
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-02 20:38:47 +0900

    feat: form 유효성 검증 모드 all로 변경

--- frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
+++ frontend/src/components/onboarding/store-register/store-register-form/StoreRegisterForm.tsx
@@ -2,6 +2,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 
 import { STORE_REGISTER_STEP } from '@/constants/onboarding/store-register';
 import { useStoreRegisterStepContext } from '@/hooks/onboarding/store-register';
+import type { StoreRegisterForm as StoreRegisterFormType } from '@/types/onboarding/store-register';
 
 import {
   StoreRegisterFormContent,
@@ -10,7 +11,9 @@ import {
 
 export const StoreRegisterForm = () => {
   // TODO: 폼 type 타입 정의하기
-  const methods = useForm();
+  const methods = useForm<StoreRegisterFormType>({
+    mode: 'all',
+  });
   const { currentStep, moveNextStep } = useStoreRegisterStepContext();
 
   const handleSubmit = methods.handleSubmit(() => {
