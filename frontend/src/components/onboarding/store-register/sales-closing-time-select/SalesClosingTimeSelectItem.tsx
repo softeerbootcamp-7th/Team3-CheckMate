54be89d486ecf548061561d4addc6c733839b090
YEONGJAE LEE <71895560+lee0jae330@users.noreply.github.com>
2026-02-02 23:53:54 +0900

    [FE] 포스 연동 페이지 안내, 로딩, 성공, 실패 페이지 UI 구현 (#167)

--- frontend/src/components/onboarding/store-register/sales-closing-time-select/SalesClosingTimeSelectItem.tsx
+++ frontend/src/components/onboarding/store-register/sales-closing-time-select/SalesClosingTimeSelectItem.tsx
@@ -13,7 +13,6 @@ export const SalesClosingTimeSelectItem = ({
   const formattedTime = `${time.toString()}시`;
   return (
     <SelectItem
-      key={time}
       value={`${time.toString()}시`}
       className={cn(
         'body-medium-medium! text-grey-700 bg-grey-100 hover:bg-grey-300 active:bg-grey-500 active:text-grey-50 flex h-9 cursor-pointer items-center justify-center rounded-none',
