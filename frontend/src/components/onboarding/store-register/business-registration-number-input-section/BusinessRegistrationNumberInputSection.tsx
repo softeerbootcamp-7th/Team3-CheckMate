c08dd48b419e9d191093e1b31f4c7eba1005f6f4
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-02 20:42:05 +0900

    feat: 사업자 등록 번호 input 컴포넌트와 검증 api 연동

--- frontend/src/components/onboarding/store-register/business-registration-number-input-section/BusinessRegistrationNumberInputSection.tsx
+++ frontend/src/components/onboarding/store-register/business-registration-number-input-section/BusinessRegistrationNumberInputSection.tsx
@@ -1,17 +1,109 @@
+import { useRef } from 'react';
+import { useController, useFormContext } from 'react-hook-form';
+
+import { useMutation } from '@tanstack/react-query';
+
 import { Input } from '@/components/shared';
+import { Button } from '@/components/shared/shadcn-ui';
+import { STORE_REGISTER_FORM_FIELD } from '@/constants/onboarding/store-register';
+import { postBusinessRegistrationNumber } from '@/services/onboarding/store-register';
+import type { StoreRegisterForm } from '@/types/onboarding/store-register';
+import { validateBusinessRegistrationNumber } from '@/utils/onboarding/store-register';
+import { cn } from '@/utils/shared';
 
 import { StoreRegisterFormTitle } from '../store-register-form-title';
-import { StoreRegisterStepButtonGroup } from '../store-register-step-button-group';
 
 export const BusinessRegistrationNumberInputSection = () => {
+  const { control, setValue, watch } = useFormContext<StoreRegisterForm>();
+  const {
+    field: { ref, onChange, onBlur, value },
+    fieldState: { error, isTouched, invalid },
+  } = useController({
+    name: STORE_REGISTER_FORM_FIELD.BUSINESS_REGISTRATION_NUMBER,
+    control,
+    rules: {
+      validate: validateBusinessRegistrationNumber,
+    },
+    defaultValue: '',
+  });
+
+  const businessAuthToken = watch(
+    STORE_REGISTER_FORM_FIELD.BUSINESS_AUTH_TOKEN,
+  );
+
+  const { mutate } = useMutation({
+    mutationFn: postBusinessRegistrationNumber,
+    onSuccess: ({ businessAuthToken }) => {
+      setValue(
+        STORE_REGISTER_FORM_FIELD.BUSINESS_AUTH_TOKEN,
+        businessAuthToken,
+      );
+    },
+    onError: (error) => {
+      console.error(error);
+    },
+  });
+
+  const verifyButtonRef = useRef<HTMLButtonElement>(null);
+
+  const handleVerifyBusinessRegistrationNumber = () => {
+    mutate({
+      businessRegistrationNumber: value,
+    });
+  };
+
+  const handlePreventEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      verifyButtonRef.current?.click();
+    }
+  };
+
+  // error가 있고 한번 이상 blur된 경우
+  const isError = !!error && isTouched;
+
+  // error가 있거나 값이 없는 경우
+  const isDisabled = invalid || !value;
+
   return (
     <>
       <StoreRegisterFormTitle
         title={`안녕하세요 사장님! \n 매장 등록을 시작할게요`}
       />
-      <div className="flex w-full flex-col gap-8">
-        <Input label="매장 사업자등록번호" placeholder="-없이 숫자만 입력" />
-        <StoreRegisterStepButtonGroup />
+      <div
+        className={cn('flex w-full flex-col', isError ? 'gap-0.5' : 'gap-8')}
+      >
+        <Input
+          label="매장 사업자등록번호"
+          placeholder="-없이 숫자만 입력"
+          ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value}
+          isError={isError && isTouched}
+          errorMessage={error?.message}
+          onKeyDown={handlePreventEnter}
+        />
+        {businessAuthToken ? (
+          <div>
+            <p>사업자 인증 성공</p>
+          </div>
+        ) : (
+          <Button
+            type="button"
+            className={cn(
+              'rounded-150 bg-grey-100 title-small-semibold! text-grey-500 flex h-12 w-20 items-center justify-center self-end px-3 py-2',
+              isDisabled
+                ? 'bg-grey-100 text-grey-500'
+                : 'text-grey-50 bg-brand-main',
+            )}
+            onClick={handleVerifyBusinessRegistrationNumber}
+            disabled={isDisabled}
+            ref={verifyButtonRef}
+          >
+            조회
+          </Button>
+        )}
       </div>
     </>
   );
