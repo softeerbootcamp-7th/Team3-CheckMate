0b3ba6e9a1dc4c82b63a46695ba7e37f3bc8d1d3
minseon <mskwon0601@gmail.com>
2026-02-03 20:13:21 +0900

    feat: toast 컴포넌트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from 'sonner';
+
 import { PageRouter } from '@/routes';
 
 function App() {
   return (
     <div className="h-screen w-screen">
       <PageRouter />
+      <Toaster offset={{ bottom: '250px' }} />
     </div>
   );
 }
