a68c7f4718b2fc0cf2c2655bfa3b973912ae9202
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-04 15:25:26 +0900

    feat: 매장명 관련 조건들 상수로 정의

--- frontend/src/utils/onboarding/store-register/validateStoreName.ts
+++ frontend/src/utils/onboarding/store-register/validateStoreName.ts
@@ -1,12 +1,12 @@
-export const MAX_STORE_NAME_LENGTH = 15;
+import { STORE_NAME } from '@/constants/onboarding/store-register';
 
 export const validateStoreName = (value: string) => {
   if (!value) {
     return '매장명을 입력해주세요.';
   }
 
-  if (value.length > MAX_STORE_NAME_LENGTH) {
-    return `매장명은 ${MAX_STORE_NAME_LENGTH}자 이내로 입력해주세요.`;
+  if (value.length > STORE_NAME.MAX_LENGTH) {
+    return `매장명은 ${STORE_NAME.MAX_LENGTH}자 이내로 입력해주세요.`;
   }
 
   return true;
