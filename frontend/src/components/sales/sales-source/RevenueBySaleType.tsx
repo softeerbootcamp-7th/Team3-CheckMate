bbc2142b94929f8d1a6dd68f6069d8d8b36dba59
lwjmcn <clarecse02@gmail.com>
2026-02-04 21:40:20 +0900

    feat: 매출 분석 페이지에 도넛 차트 추가

--- frontend/src/components/sales/sales-source/RevenueBySaleType.tsx
+++ frontend/src/components/sales/sales-source/RevenueBySaleType.tsx
@@ -1,8 +1,14 @@
+import { DoughnutChart } from '@/components/shared';
+import { SALES_SOURCE_DATA } from '@/mocks/data/sales';
+
 export const RevenueBySaleType = () => {
   return (
     <article className="card">
       <h3>판매유형별 매출</h3>
-      <div>Types: 0</div>
+
+      <div className="mx-auto my-4.5 size-45">
+        <DoughnutChart chartData={SALES_SOURCE_DATA} />
+      </div>
     </article>
   );
 };
