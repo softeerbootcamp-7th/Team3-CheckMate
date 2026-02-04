462865766fb018a2685b1c54d409f4e6f2de2746
minseon <mskwon0601@gmail.com>
2026-02-04 17:21:50 +0900

    setting 페이지에 식자재 입력모달 창 pop 할 수 있는 카드 위치

--- frontend/src/pages/setting-page/SettingPage.tsx
+++ frontend/src/pages/setting-page/SettingPage.tsx
@@ -3,12 +3,21 @@ import { useState } from 'react';
 import { IngredientEditDialog } from '@/components/ingredient';
 
 export const SettingPage = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState(true);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   return (
-    <IngredientEditDialog
-      open={isDialogOpen}
-      onOpenChange={setIsDialogOpen}
-      menuId="28"
-    />
+    <div>
+      <div
+        className="bg-special-card-bg rounded-200 flex h-[200px] w-[256px] cursor-pointer flex-col justify-between p-6"
+        onClick={() => setIsDialogOpen(true)}
+      >
+        <p className="title-small-bold">딸기 스무디</p>
+        <p className="text-end">5800 원</p>
+      </div>
+      <IngredientEditDialog
+        open={isDialogOpen}
+        onOpenChange={setIsDialogOpen}
+        menuId="28"
+      />
+    </div>
   );
 };
