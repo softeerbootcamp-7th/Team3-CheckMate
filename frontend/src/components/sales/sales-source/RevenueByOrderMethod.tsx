69e7108fc709d585c326c6381f4e78539a280e13
lwjmcn <clarecse02@gmail.com>
2026-02-05 19:58:33 +0900

    feat: 카테고리 매출 차트 범례 컴포넌트 추가
    - 라벨에 따라 색상이 지정되지 않고, 랭킹에 따른 색상을 사용함

--- frontend/src/components/sales/sales-source/RevenueByOrderMethod.tsx
+++ frontend/src/components/sales/sales-source/RevenueByOrderMethod.tsx
@@ -1,8 +1,34 @@
+import { useMemo, useState } from 'react';
+
+import { CategoryRevenueChartLegend } from '@/components/menu/CategoryRevenueChartLegend';
+import { DoughnutChart } from '@/components/shared/doughnut-chart/DoughnutChart';
+import { CATEGORIES_RANKING_DATA } from '@/mocks/data/menu';
+import type { CategoriesRevenue } from '@/types/menu/categoriesRevenue';
+import type { DoughnutChartItem } from '@/types/shared';
+
 export const RevenueByOrderMethod = () => {
+  const [categoriesRevenueData] = useState<CategoriesRevenue[]>(
+    CATEGORIES_RANKING_DATA,
+  );
+
+  const chartData: DoughnutChartItem[] = useMemo(
+    () =>
+      categoriesRevenueData
+        .sort((a, b) => b.revenue - a.revenue)
+        .map((item) => ({
+          label: item.category,
+          value: item.revenue,
+        })),
+    [categoriesRevenueData],
+  );
+
   return (
     <article className="card">
       <h3>주문수단별 매출</h3>
-      <div>Types: 0</div>
+      <div className="mx-auto my-4.5 size-45">
+        <DoughnutChart chartData={chartData} />
+      </div>
+      <CategoryRevenueChartLegend chartData={chartData} />
     </article>
   );
 };
---
+++ frontend/src/constants/shared/doughnut-chart/index.ts
@@ -0,0 +1,1 @@
+export { RANKING_COLORS } from './rankingColors';
