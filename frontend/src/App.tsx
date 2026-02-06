dba4fc52bd7b40328895f51e3a1728ec1ada07fb
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-06 10:52:22 +0900

    chore: toast import 경로 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Toaster } from 'sonner';
 
+import { Toaster } from '@/components/shared/shadcn-ui';
 import { PageRouter } from '@/routes';
 import { queryClient } from '@/services/shared';
 
