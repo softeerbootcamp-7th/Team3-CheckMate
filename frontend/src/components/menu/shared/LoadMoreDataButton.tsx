016b8c5c9fa409a500b48b0c619ab3b7625baea1
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-08 01:48:45 +0900

    chore: 더보기 버튼 Link 태그 기반 페이지 이동으로 변경

--- frontend/src/components/menu/shared/LoadMoreDataButton.tsx
+++ frontend/src/components/menu/shared/LoadMoreDataButton.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { Button } from '@/components/shared/shadcn-ui';
 
@@ -7,16 +7,13 @@ interface LoadMoreDataButtonProps {
 }
 
 export const LoadMoreDataButton = ({ path }: LoadMoreDataButtonProps) => {
-  const navigate = useNavigate();
-  const handleClickLoadMore = () => {
-    navigate(path);
-  };
   return (
     <Button
       className="rounded-200 bg-grey-100 text-grey-600 body-medium-semibold! flex h-10 w-full items-center justify-center py-2.5"
-      onClick={handleClickLoadMore}
+      type="button"
+      asChild
     >
-      더보기
+      <Link to={path}>더보기</Link>
     </Button>
   );
 };
