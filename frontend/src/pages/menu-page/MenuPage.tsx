48cffc22b379568d69543435e86a7750a1e31748
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-07 20:43:13 +0900

    feat: 인기 메뉴 관련 메뉴 분석 컴포넌트 추가

--- frontend/src/pages/menu-page/MenuPage.tsx
+++ frontend/src/pages/menu-page/MenuPage.tsx
@@ -1,3 +1,8 @@
+import { PopularMenuOverview } from '@/components/menu';
 export const MenuPage = () => {
-  return <div>MenuPage</div>;
+  return (
+    <div className="flex size-full flex-col gap-12 py-32.5">
+      <PopularMenuOverview />
+    </div>
+  );
 };
