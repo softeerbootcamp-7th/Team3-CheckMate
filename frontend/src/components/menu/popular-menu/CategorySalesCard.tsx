407d225fa5a77e2a01ee3e97b3391f50c4e84d63
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-07 20:42:52 +0900

    feat: 카테고리별 메뉴 매출 카드 추가

---
+++ frontend/src/components/menu/popular-menu/CategorySalesCard.tsx
@@ -0,0 +1,14 @@
+import { MenuAnalysisCard } from '../shared';
+
+// TODO: 도넛 차트 추가 필요
+export const CategorySalesCard = () => {
+  return (
+    <MenuAnalysisCard
+      aria-label="카테고리별 매출"
+      className="flex h-80 flex-1 flex-col gap-12"
+      title="카테고리별 매출"
+    >
+      <div>카테고리별 메출 도넛차트 위치</div>
+    </MenuAnalysisCard>
+  );
+};
