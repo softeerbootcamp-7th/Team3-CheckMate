1a4dcb392da7abf7a254a9427abddad0e14da558
lwjmcn <clarecse02@gmail.com>
2026-02-08 20:51:23 +0900

    feat: 기간 선택 훅 사용을 위한 매출 유입 구조 헤더 컴포넌트 분리

--- frontend/src/components/sales/sales-source/SalesSource.tsx
+++ frontend/src/components/sales/sales-source/SalesSource.tsx
@@ -1,27 +1,20 @@
-import { CategoryRevenueRanking } from '@/components/menu/CategoryRevenueRanking';
-import { SectionTitle } from '@/components/shared';
-
+import { PeriodTypeProvider } from './period-type-provider';
 import { RevenueByOrderMethod } from './RevenueByOrderMethod';
 import { RevenueByPaymentMethod } from './RevenueByPaymentMethod';
 import { RevenueBySaleType } from './RevenueBySaleType';
+import { SalesSourceHeader } from './SalesSourceHeader';
 
 export const SalesSource = () => {
   return (
     <section aria-label="매출 유입 구조">
-      <header>
-        <SectionTitle
-          title="매출 유입 구조"
-          description="매출이 어떤 경로와 방식으로 들어왔는지 확인해요."
-        />
-      </header>
-      <section className="mt-4 grid grid-cols-3 gap-5">
-        <RevenueBySaleType />
-        <RevenueByOrderMethod />
-        <RevenueByPaymentMethod />
-
-        {/* 카테고리별 매출 테스트용 */}
-        <CategoryRevenueRanking />
-      </section>
+      <PeriodTypeProvider>
+        <SalesSourceHeader />
+        <section className="mt-4 grid grid-cols-3 gap-5">
+          <RevenueBySaleType />
+          <RevenueByOrderMethod />
+          <RevenueByPaymentMethod />
+        </section>
+      </PeriodTypeProvider>
     </section>
   );
 };
