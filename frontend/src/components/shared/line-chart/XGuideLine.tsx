d0426461e7fc6b0231e7fd683d61a458242b656b
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-10 01:33:58 +0900

    feat: x축 가이드라인 컴포넌트 추가

---
+++ frontend/src/components/shared/line-chart/XGuideLine.tsx
@@ -0,0 +1,51 @@
+import { LINE_CHART } from '@/constants/shared';
+
+interface XGuideLineProps {
+  cordinate: (number | null)[][];
+  svgRect: DOMRect | null;
+  adjustedHeight: number;
+}
+
+export const XGuideLine = ({
+  cordinate,
+  svgRect,
+  adjustedHeight,
+}: XGuideLineProps) => {
+  if (svgRect === null) {
+    return null;
+  }
+
+  const { TICK_HEIGHT, GUIDE_LINE_STROKE_WIDTH } = LINE_CHART;
+
+  const xGuideLinePathD = cordinate
+    .map(([x]) => {
+      return `M ${x} 0 v ${adjustedHeight - TICK_HEIGHT / 2}`;
+    })
+    .join(' ');
+
+  const xGuideTickPathD = cordinate
+    .map(([x]) => {
+      return `M ${x} ${adjustedHeight - TICK_HEIGHT / 2} v ${TICK_HEIGHT}`;
+    })
+    .join(' ');
+  return (
+    <>
+      <path
+        d={xGuideLinePathD}
+        strokeWidth={GUIDE_LINE_STROKE_WIDTH}
+        stroke="currentColor"
+        strokeDasharray="4 4"
+        className="text-grey-300"
+        aria-hidden="true"
+      />
+      <path
+        d={xGuideTickPathD}
+        strokeWidth={GUIDE_LINE_STROKE_WIDTH}
+        stroke="currentColor"
+        strokeLinecap="round"
+        className="text-grey-400"
+        aria-hidden="true"
+      />
+    </>
+  );
+};
