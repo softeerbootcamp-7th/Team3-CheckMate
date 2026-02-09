6ba64793255b9a1afe5a265467643627be64647f
lwjmcn <clarecse02@gmail.com>
2026-02-09 18:27:37 +0900

    feat: 하루리포트 헤더 소제목 수정

--- frontend/src/components/daily-report/DailyReportHeader.tsx
+++ frontend/src/components/daily-report/DailyReportHeader.tsx
@@ -3,14 +3,11 @@ import { SectionTitle } from '@/components/shared';
 import { NotificationDialogTrigger } from './notification-dialog';
 
 export const DailyReportHeader = () => {
-  const relativeDate = '{상대일자}';
-  const timeRange = '{시간범위}';
-
   return (
     <header className="flex justify-between">
       <SectionTitle
         title="하루 리포트"
-        description={`다음 하루 리포트는 ${relativeDate} ${timeRange}에 발행돼요.`}
+        description={`하루 리포트는 마감시간 기준 한 시간 내에 발행돼요.`}
       />
       <NotificationDialogTrigger />
     </header>
