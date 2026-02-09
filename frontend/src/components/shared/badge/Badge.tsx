29817030f2948b777f2742f35014f4c29aedaefd
lwjmcn <clarecse02@gmail.com>
2026-02-09 16:38:27 +0900

    style: 알림 뱃지 top left 위치 스타일링 변경

--- frontend/src/components/shared/badge/Badge.tsx
+++ frontend/src/components/shared/badge/Badge.tsx
@@ -17,7 +17,7 @@ export const Badge = ({
       className={cn(
         'before:bg-brand-main relative before:absolute before:size-[6px] before:rounded-full',
         !show && 'before:opacity-0',
-        position === 'top-left' && 'before:-top-px before:-left-px',
+        position === 'top-left' && 'before:-top-px before:-left-1.25',
         position === 'top-right' && 'before:-top-px before:-right-px',
         position === 'right' &&
           'before:top-[50%] before:-right-2.5 before:translate-y-[-0.5px]',
