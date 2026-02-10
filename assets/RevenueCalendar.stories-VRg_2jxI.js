54963663c4ddd9772a771a7e174d879d23e9db49
lwjmcn <lwjmcn@users.noreply.github.com>
2026-02-10 10:35:02 +0000

    deploy: 7b896d83f50a297b656b443d3d6bd4529500470b

---
+++ assets/RevenueCalendar.stories-VRg_2jxI.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-u17CrQMm.js";import{r as n}from"./iframe-scyTIVvh.js";import{R as a}from"./RevenueCalendar-_oem-bvr.js";import"./preload-helper-Cv0CY36J.js";import"./periods-CKiPmzQI.js";import"./index-BSRdqYYM.js";import"./index-Eu5c4rvX.js";import"./index-D1t1vmSF.js";import"./chevron-right-DkUsNthZ.js";import"./createLucideIcon-CE3Lex0H.js";const S={title:"components/shared/calendar/RevenueCalendar",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{selectedDate:{control:"date"}}},d=t=>{const[s,o]=n.useState(t.selectedDate);return r.jsx(a,{selectedDate:s,setSelectedDate:o})},e={args:{selectedDate:void 0,setSelectedDate:()=>{}},render:t=>r.jsx(d,{...t})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    selectedDate: undefined,
+    setSelectedDate: () => {}
+  },
+  render: args => {
+    return <DefaultRevenueCalendarStory {...args} />;
+  }
+}`,...e.parameters?.docs?.source}}};const R=["Default"];export{e as Default,R as __namedExportsOrder,S as default};
---
+++ assets/SignInButton.stories-C-C6PAk8.js
@@ -0,0 +1,6 @@
+import{j as o}from"./jsx-runtime-u17CrQMm.js";import{S as r}from"./SignInButton-BbAYNi-x.js";import"./index-BSRdqYYM.js";import"./iframe-scyTIVvh.js";import"./preload-helper-Cv0CY36J.js";import"./periods-CKiPmzQI.js";import"./index-Eu5c4rvX.js";import"./index-D1t1vmSF.js";const{fn:e}=__STORYBOOK_MODULE_TEST__,d={title:"components/sign-in/sign-in-button/SignInButton",component:r,parameters:{layout:"centered"},tags:["autodocs"]},t={args:{onClick:e()},render:n=>o.jsx(r,{...n})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    onClick: fn()
+  },
+  render: args => <SignInButton {...args} />
+}`,...t.parameters?.docs?.source}}};const l=["Default"];export{t as Default,l as __namedExportsOrder,d as default};
