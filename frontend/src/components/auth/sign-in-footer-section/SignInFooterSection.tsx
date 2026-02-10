e6b2a0f88a266fda2b379ae4b44f49af5da1ef66
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-10 17:40:51 +0900

    feat: 개인정보처리방침 페이지 추가

--- frontend/src/components/auth/sign-in-footer-section/SignInFooterSection.tsx
+++ frontend/src/components/auth/sign-in-footer-section/SignInFooterSection.tsx
@@ -1,21 +1,17 @@
 import { Link } from 'react-router-dom';
 
-import { CDN_BASE_URL } from '@/constants/shared';
+import { ROUTE_PATHS } from '@/constants/shared';
 
 export const SignInFooterSection = () => {
   return (
     <section className="flex size-full flex-col items-center justify-center gap-3">
       <div className="text-grey-700 body-medium-medium flex items-center gap-3">
-        <Link
-          to={`${CDN_BASE_URL}/public/term.html`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link to={ROUTE_PATHS.TERMS} target="_blank" rel="noopener noreferrer">
           이용약관
         </Link>
         <div className="bg-grey-400 h-4 w-px" />
         <Link
-          to={`${CDN_BASE_URL}/public/privacy.html`}
+          to={ROUTE_PATHS.PRIVACY}
           target="_blank"
           rel="noopener noreferrer"
         >
