aba330c3c6eeeb320b065c9856f5d54349d47040
minseon <mskwon0601@gmail.com>
2026-02-10 20:11:26 +0900

    chore: StoreNameInput에 readOnly 속성 추가해서 커서 깜빡이는거 삭제

--- frontend/src/components/setting/setting-my-store-name/SettingMyStoreName.tsx
+++ frontend/src/components/setting/setting-my-store-name/SettingMyStoreName.tsx
@@ -9,10 +9,6 @@ export const SettingMyStoreName = ({ storeName }: SettingMyStoreNameProps) => {
   // 서버에서 받아온 값으로 RHF 폼 값을 초기화(덮어쓰기)
   useInitializeStoreName({ storeName });
   // 처음 환경설정 창 들어가면 매장명 입력란에 focus가서 커서 깜빡거리는 문제 발생
-  // 깜빡이는 커서 이름이 caret임 -> 투명색으로 설정해 안보이게...
-  return (
-    <div className="[&_input]:caret-transparent">
-      <StoreNameInput />
-    </div>
-  );
+  // readOnly 속성 추가하여 읽기 전용으로 변경
+  return <StoreNameInput readOnly />;
 };
