185bcbe7449ac449b51d90979e7f48047726ba79
minseon <mskwon0601@gmail.com>
2026-02-10 20:13:20 +0900

    chore: 만든 에러 바운더리 적용

--- frontend/src/components/setting/setting-my-store-info/SettingMyStoreInfo.tsx
+++ frontend/src/components/setting/setting-my-store-info/SettingMyStoreInfo.tsx
@@ -1,31 +1,16 @@
 import { FormProvider } from 'react-hook-form';
 
-import { Spinner } from '@/components/shared';
 import { useStoreRegisterForm } from '@/hooks/onboarding/store-register';
 import { useSettingMyStoreInfo } from '@/hooks/setting';
 
-import { StateWrapper } from '../../shared/state-wrapper';
 import { SettingMyStoreBusinessHours } from '../setting-my-store-business-hours';
 import { SettingMyStoreName } from '../setting-my-store-name';
 import { SettingMyStoreSalesClosingTime } from '../setting-my-store-sales-closing-time';
 
 export const SettingMyStoreInfo = () => {
-  const { data, isPending, error } = useSettingMyStoreInfo();
+  const { data } = useSettingMyStoreInfo();
   const { methods } = useStoreRegisterForm();
-  if (isPending) {
-    return (
-      <StateWrapper width={1040} height={590}>
-        <Spinner className="size-8" />
-      </StateWrapper>
-    );
-  }
-  if (error || !data) {
-    return (
-      <StateWrapper width={1040} height={590}>
-        <span>매장 정보를 불러오지 못했습니다. 다시 시도해 주세요</span>
-      </StateWrapper>
-    );
-  }
+
   return (
     <FormProvider {...methods}>
       <section className="rounded-400 bg-special-card-bg w-260 px-8 pt-6 pb-9">
