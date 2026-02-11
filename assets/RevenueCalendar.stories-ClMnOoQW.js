bb3cf1cd929795ac02ecb57795a1a208f2b14e38
lee0jae330 <lee0jae330@users.noreply.github.com>
2026-02-11 03:34:34 +0000

    deploy: 5977afad4c66d565a6d72ff3e898be18a11a32c4

--- assets/RevenueCalendar.stories-ClMnOoQW.js
+++
@@ -1,9 +0,0 @@
-import{j as r}from"./jsx-runtime-u17CrQMm.js";import{r as n}from"./iframe-DefvGnge.js";import{R as a}from"./RevenueCalendar-B-yG06bP.js";import"./preload-helper-Cv0CY36J.js";import"./periods-CKiPmzQI.js";import"./index-DPFuuXwE.js";import"./index-593xL0pz.js";import"./index-yDaA8qkC.js";import"./chevron-right-BzAZjc7w.js";import"./createLucideIcon-D2Wq38FW.js";const S={title:"components/shared/calendar/RevenueCalendar",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{selectedDate:{control:"date"}}},d=t=>{const[s,o]=n.useState(t.selectedDate);return r.jsx(a,{selectedDate:s,setSelectedDate:o})},e={args:{selectedDate:void 0,setSelectedDate:()=>{}},render:t=>r.jsx(d,{...t})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    selectedDate: undefined,
-    setSelectedDate: () => {}
-  },
-  render: args => {
-    return <DefaultRevenueCalendarStory {...args} />;
-  }
-}`,...e.parameters?.docs?.source}}};const R=["Default"];export{e as Default,R as __namedExportsOrder,S as default};
--- assets/SignInMainSection.stories-D8HGWXwa.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{S as o}from"./SignInMainSection-DH13MFrq.js";import"./FeatureCard-C_ACkN2b.js";import"./SignInButton-xJR0pEHk.js";import"./index-DPFuuXwE.js";import"./iframe-DefvGnge.js";import"./preload-helper-Cv0CY36J.js";import"./periods-CKiPmzQI.js";import"./index-593xL0pz.js";import"./index-yDaA8qkC.js";const{fn:s}=__STORYBOOK_MODULE_TEST__,u={title:"components/sign-in/sign-in-main-section/SignInMainSection",component:o,parameters:{layout:"fullscreen"},tags:["autodocs"]},r={decorators:[n=>e.jsx("div",{className:"h-screen w-screen",children:e.jsx(n,{})})],args:{onClick:s()},render:n=>e.jsx(o,{...n})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  decorators: [Story => <div className="h-screen w-screen">
-        <Story />
-      </div>],
-  args: {
-    onClick: fn()
-  },
-  render: args => <SignInMainSection {...args} />
-}`,...r.parameters?.docs?.source}}};const f=["Default"];export{r as Default,f as __namedExportsOrder,u as default};
