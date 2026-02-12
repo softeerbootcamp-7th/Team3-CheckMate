170d2aa89325439580890716567a9301b4c5fb35
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-12 22:24:15 +0900

    fix: resolve build failed

--- frontend/src/components/onboarding/pos-integration/pos-integration-success-section/DashboardNavigateButton.tsx
+++ frontend/src/components/onboarding/pos-integration/pos-integration-success-section/DashboardNavigateButton.tsx
@@ -9,7 +9,7 @@ export const DashboardNavigateButton = () => {
       className="rounded-150 bg-brand-main text-grey-50 title-small-semibold! flex h-14 w-55 items-center justify-center"
       asChild
     >
-      <Link to={ROUTE_PATHS.DASHBOARD} replace>
+      <Link to={ROUTE_PATHS.DASHBOARD.BASE} replace>
         대시보드 바로가기
       </Link>
     </Button>
