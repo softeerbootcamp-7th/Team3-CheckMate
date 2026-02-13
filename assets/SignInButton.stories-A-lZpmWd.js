5072a2e7c3635a01eddf5d6dca51e6ed5a16be43
lwjmcn <lwjmcn@users.noreply.github.com>
2026-02-13 03:24:28 +0000

    deploy: f12c89cb81383e962ff890d9ad7cec9875325cb5

---
+++ assets/SignInButton.stories-A-lZpmWd.js
@@ -0,0 +1,6 @@
+import{j as n}from"./jsx-runtime-u17CrQMm.js";import{S as r}from"./SignInButton-ChJ-_9Sq.js";import"./index-ZBpQw2zg.js";import"./iframe-DnnDCraO.js";import"./preload-helper-Cv0CY36J.js";import"./periods-BueKGNxC.js";import"./index-GF96FoPb.js";import"./index-hQh9mggH.js";import"./index-D8qwCdCv.js";const{fn:e}=__STORYBOOK_MODULE_TEST__,l={title:"components/sign-in/sign-in-button/SignInButton",component:r,parameters:{layout:"centered"},tags:["autodocs"]},t={args:{onClick:e()},render:o=>n.jsx(r,{...o})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    onClick: fn()
+  },
+  render: args => <SignInButton {...args} />
+}`,...t.parameters?.docs?.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,l as default};
