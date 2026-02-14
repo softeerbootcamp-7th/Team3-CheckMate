3d6f7ff44cf04523a318d9610445fcc943397ed6
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-14 16:39:19 +0900

    feat: 실매출 관련 상수 정의

--- frontend/src/constants/sales/index.ts
+++ frontend/src/constants/sales/index.ts
@@ -1,4 +1,4 @@
 export { SALES_SOURCE_COLORS } from './salesSource';
 export type { SalesSourceType } from './salesSource';
 export { SALES_UNIT } from './salesUnit';
-export { CURRENT_SALES } from './currentSales';
+export { REAL_SALES } from './realSales';
