baf029ce03964e016944c41f0db57d4b1fc6bb79
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-14 16:39:49 +0900

    chore: 지표 경향성 계산 관련 유틸 메서드 args interface 분리

--- frontend/src/utils/dashboard/getMetricTrend.ts
+++ frontend/src/utils/dashboard/getMetricTrend.ts
@@ -1,9 +1,19 @@
 import { METRIC_TREND } from '@/constants/dashboard';
 
-export const getMetricTrend = (differentAmount: number) => {
-  if (differentAmount > 0) {
+interface GetMetricTrendArgs {
+  comparisonAmount: number;
+  minValue: number;
+  maxValue: number;
+}
+
+export const getMetricTrend = ({
+  comparisonAmount,
+  minValue,
+  maxValue,
+}: GetMetricTrendArgs) => {
+  if (comparisonAmount >= maxValue) {
     return METRIC_TREND.UP;
-  } else if (differentAmount < 0) {
+  } else if (comparisonAmount <= minValue) {
     return METRIC_TREND.DOWN;
   } else {
     return METRIC_TREND.SAME;
