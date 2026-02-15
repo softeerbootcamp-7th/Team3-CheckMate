c181812322c09a6acad87591a82716d914a2ea9e
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-16 03:07:06 +0900

    refactor: lineChart Dots에서 Dot 컴포너트 분리

---
+++ frontend/src/components/shared/line-chart/Dot.tsx
@@ -0,0 +1,37 @@
+import { LINE_CHART } from '@/constants/shared';
+import { cn } from '@/utils/shared';
+
+interface DotProps {
+  x: number;
+  y: number;
+  color: string;
+  hasHoverEffect?: boolean;
+  ariaLabel: string;
+}
+
+export const Dot = ({
+  x,
+  y,
+  color,
+  ariaLabel,
+  hasHoverEffect = false,
+}: DotProps) => {
+  const { DOT_RADIUS } = LINE_CHART;
+
+  return (
+    <circle
+      cx={x}
+      cy={y}
+      r={DOT_RADIUS}
+      fill={color}
+      stroke="none"
+      role="graphics-symbol"
+      tabIndex={-1}
+      aria-label={ariaLabel}
+      className={cn(hasHoverEffect && 'hover:brightness-75 hover:saturate-200')}
+      style={{
+        transition: 'cx 0.5s ease-in-out, cy 0.5s ease-in-out',
+      }}
+    />
+  );
+};
