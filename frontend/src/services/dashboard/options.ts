d9b7fc122504c41f4aac7d0f2279384fa90d5c65
lwjmcn <clarecse02@gmail.com>
2026-02-18 01:13:33 +0900

    fix: options에 잘못된 key 사용

--- frontend/src/services/dashboard/options.ts
+++ frontend/src/services/dashboard/options.ts
@@ -5,7 +5,7 @@ import { dashboardKeys } from './keys';
 
 export const dashboardOptions = {
   list: queryOptions({
-    queryKey: dashboardKeys.all,
+    queryKey: dashboardKeys.list(),
     queryFn: getDashboardList,
     staleTime: 24 * 60 * 60 * 1000, // 24시간
   }),
