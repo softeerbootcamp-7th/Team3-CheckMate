63557599193c20d93fc4d9bf905dfe3244f4ea29
lwjmcn <clarecse02@gmail.com>
2026-02-18 02:23:42 +0900

    feat: local storage에 현재 대시보드 id 저장

--- frontend/src/components/dashboard/dashboard-tabs-provider/DashboardTabsProvider.tsx
+++ frontend/src/components/dashboard/dashboard-tabs-provider/DashboardTabsProvider.tsx
@@ -11,9 +11,36 @@ import { dashboardOptions } from '@/services/dashboard';
 export const DashboardTabsProvider = ({ children }: PropsWithChildren) => {
   const { data: tabs } = useSuspenseQuery(dashboardOptions.list);
 
-  const [currentDashboardId, setCurrentDashboardId] = useState<number>(
-    () => tabs.find((tab) => tab.isDefault)?.id ?? 1,
+  // localStorage에 대시보드 ID 저장
+  const STORAGE_KEY = 'dashboard_current_id';
+  const getSavedDashboardId = () => {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      const parsedId = Number(localStorage.getItem(STORAGE_KEY));
+      if (tabs.some((t) => t.id === parsedId)) {
+        return parsedId;
+      }
+    }
+  };
+  const saveDashboardId = (id: number) => {
+    try {
+      localStorage.setItem(STORAGE_KEY, String(id));
+    } catch (e) {
+      if (e instanceof Error) {
+        throw new Error(e.message);
+      } else {
+        throw new Error('선택한 대시보드 정보를 저장할 수 없어요.');
+      }
+    }
+  };
+
+  const [currentDashboardId, setCurrentDashboardIdState] = useState<number>(
+    getSavedDashboardId() ?? tabs.find((tab) => tab.isDefault)?.id ?? 1,
   );
+  const setCurrentDashboardId = (id: number) => {
+    setCurrentDashboardIdState(id);
+    saveDashboardId(id);
+  };
+
   const [dialogState, setDialogState] = useState<{
     open: boolean;
     mode: DashboardTabsDialogMode | null;
