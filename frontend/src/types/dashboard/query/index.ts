97548f888966621a6986400decb630937f702f3d
lwjmcn <clarecse02@gmail.com>
2026-02-17 18:08:32 +0900

    chore: dto, query 타입 배럴 파일 추가

---
+++ frontend/src/types/dashboard/query/index.ts
@@ -0,0 +1,4 @@
+export type { PatchDashboardNameQuery } from './patchDashboardNameQuery';
+export type { DeleteDashboardQuery } from './deleteDashboardQuery';
+export type { GetDashboardCardListQuery } from './getDashboardCardListQuery';
+export type { PutDashboardCardListQuery } from './putDashboardCardListQuery';
