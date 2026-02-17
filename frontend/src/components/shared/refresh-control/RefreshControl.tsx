1863a1f1d268b8186d98b6d7a36bbeea6c6882e4
minseon <mskwon0601@gmail.com>
2026-02-18 07:55:56 +0900

    chore: 새로고침 시간 안내 문구 변경

--- frontend/src/components/shared/refresh-control/RefreshControl.tsx
+++ frontend/src/components/shared/refresh-control/RefreshControl.tsx
@@ -5,7 +5,7 @@ import { cn, formatDateYYMMDDHHMM } from '@/utils/shared';
 
 // 데이터 새로고침된 시간과 새로고침 버튼이 있는 컴포넌트
 interface RefreshControlProps {
-  lastUpdatedDate: Date; // 마지막으로 데이터가 새로고침된 시간
+  lastUpdatedDate: Date | null; // 마지막으로 데이터가 새로고침된 시간
   onRefresh: () => void; // 새로고침 버튼 클릭 시 호출되는 함수
   isLoading: boolean; // 데이터가 새로고침 중인지 여부
 }
@@ -19,10 +19,12 @@ export const RefreshControl = ({
       <span className="body-small-medium text-grey-500">
         {isLoading
           ? '데이터를 불러오는 중입니다'
-          : `${formatDateYYMMDDHHMM(lastUpdatedDate)} 기준`}
+          : lastUpdatedDate
+            ? `${formatDateYYMMDDHHMM(lastUpdatedDate)}에 새로고침`
+            : '업데이트 시간 정보가 없습니다'}
       </span>
       <Button
-        className="rounded-unlimit bg-grey-300 h-fit! p-0.5!"
+        className="rounded-unlimit bg-grey-300 flex h-fit! items-center justify-center p-0.5!"
         onClick={onRefresh}
         disabled={isLoading} // 새로고침 중일 때 버튼 비활성화
         aria-label="데이터 새로고침 버튼"
--- frontend/src/components/shared/section-header/SectionHeader.tsx
+++ frontend/src/components/shared/section-header/SectionHeader.tsx
@@ -7,7 +7,7 @@ interface SectionHeaderProps {
   description?: string; // 섹션에 대한 추가 설명
 
   // refreshControl 관련 props -> 새로고침된 시간, 새로고침 버튼
-  lastUpdatedDate: Date; // 마지막으로 데이터가 새로고침된 시간
+  lastUpdatedDate: Date | null; // 마지막으로 데이터가 새로고침된 시간
   onRefresh: () => void; // 새로고침 버튼 클릭 시 호출되는 함수
   isLoading: boolean; // 데이터가 새로고침 중인지 여부
 
