8cdbf4fd14aa3ea92f217c49e390da69a40bf125
lwjmcn <clarecse02@gmail.com>
2026-02-18 02:12:27 +0900

    feat: 잘못된 대시보드 접근 시 나가기 버튼 노출

--- frontend/src/components/dashboard/dashboard-edit/DashboardEditLayout.tsx
+++ frontend/src/components/dashboard/dashboard-edit/DashboardEditLayout.tsx
@@ -1,17 +1,25 @@
+import { ErrorBoundary } from 'react-error-boundary';
+
 import { FetchBoundary } from '@/components/shared';
 
 import { CardEditView } from './CardEditView';
 import { EditCardProvider } from './EditCardProvider';
+import { EditErrorFallback } from './EditErrorFallback';
 import { MiniView } from './MiniView';
 
 export const DashboardEditLayout = () => {
   return (
     <div className="flex size-full overflow-y-hidden">
       <FetchBoundary>
-        <EditCardProvider>
-          <MiniView />
-          <CardEditView />
-        </EditCardProvider>
+        <ErrorBoundary
+          fallbackRender={(props) => <EditErrorFallback {...props} />}
+        >
+          <EditCardProvider>
+            <MiniView />
+
+            <CardEditView />
+          </EditCardProvider>
+        </ErrorBoundary>
       </FetchBoundary>
     </div>
   );
