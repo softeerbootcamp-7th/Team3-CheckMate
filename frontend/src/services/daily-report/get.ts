9707b1b710cad8b40e74a5983e71aca2083acde3
lwjmcn <clarecse02@gmail.com>
2026-02-18 20:31:56 +0900

    feat: 하루리포트 관련 api 요청 함수 정의

---
+++ frontend/src/services/daily-report/get.ts
@@ -0,0 +1,34 @@
+import {
+  type GetDailyReportCalendarQuery,
+  type GetDailyReportCalendarResponseDto,
+  type GetDailyReportContentParam,
+  type GetDailyReportContentResponseDto,
+} from '@/types/daily-report';
+
+import { authorizedApi } from '../shared';
+
+export const getDailyReportContent = async (
+  param: GetDailyReportContentParam,
+) => {
+  const { date } = param;
+
+  const { data } = await authorizedApi.get<GetDailyReportContentResponseDto>(
+    `/api/reports/${date}`,
+  );
+
+  return data;
+};
+export const getDailyReportCalendar = async (
+  query: GetDailyReportCalendarQuery,
+) => {
+  const queryParams = new URLSearchParams({
+    month: query.month.toString(),
+    year: query.year.toString(),
+  }).toString();
+
+  const { data } = await authorizedApi.get<GetDailyReportCalendarResponseDto>(
+    `/api/reports/calendar?${queryParams}`,
+  );
+
+  return data;
+};
