e13bafab249ae7fbec35504c7e41eefd81290d8b
minseon <mskwon0601@gmail.com>
2026-02-18 17:22:20 +0900

    feat: 메뉴별 식재료 get하는 msw 헨들러 추가

--- frontend/src/mocks/ingredient/ingredientHandler.ts
+++ frontend/src/mocks/ingredient/ingredientHandler.ts
@@ -1,9 +1,10 @@
 import { HttpResponse } from 'msw';
 
-import { type SuccessResponse } from '@/services/shared';
+import { type ErrorResponse, type SuccessResponse } from '@/services/shared';
 import type { GetCategoryMenusResponseDto } from '@/types/ingredient';
+import type { GetMenuIngredientsResponseDto } from '@/types/ingredient/dto';
 
-import { CATEGORY_MENUS } from '../data/ingredient';
+import { CATEGORY_MENUS, INGREDIENTS_BY_MENU_ID } from '../data/ingredient';
 import { mswHttp } from '../shared';
 
 const getHandler = [
@@ -13,6 +14,32 @@ const getHandler = [
         success: true,
         message: 'Success',
         data: CATEGORY_MENUS,
+      },
+      {
+        status: 200,
+      },
+    );
+  }),
+  // 각 메뉴 별 등록된 식자재 조회 핸들러
+  mswHttp.get('/api/menus/:menuId/recipe', ({ params }) => {
+    const menuId = Number(params.menuId);
+
+    const response = INGREDIENTS_BY_MENU_ID[menuId];
+    if (!response) {
+      return HttpResponse.json<ErrorResponse>(
+        {
+          success: false,
+          message: 'Not Found',
+          errorCode: '404',
+        },
+        { status: 404 },
+      );
+    }
+    return HttpResponse.json<SuccessResponse<GetMenuIngredientsResponseDto>>(
+      {
+        success: true,
+        message: 'Success',
+        data: response,
       },
       {
         status: 200,
