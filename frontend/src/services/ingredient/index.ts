f5bed49f3504107bed0082b1b132cfa9a911f09d
minseon <mskwon0601@gmail.com>
2026-02-18 21:28:52 +0900

    chore: 식자재 등록 요청 함수 구현

--- frontend/src/services/ingredient/index.ts
+++ frontend/src/services/ingredient/index.ts
@@ -1,3 +1,4 @@
 export { getRegisteredMenus, getMenuIngredients } from './get';
 export { ingredientKeys } from './keys';
 export { postAiIngredientRecommend } from './ai-ingredient-recommend';
+export { postIngredientRegister } from './post';
---
+++ frontend/src/services/ingredient/post.ts
@@ -0,0 +1,35 @@
+import type { Ingredient } from '@/types/ingredient';
+import type { PostIngredientRegisterRequestDto } from '@/types/ingredient';
+import { convertUiUnitToServerUnit } from '@/utils/ingredient/convertUnit';
+
+import { authorizedApi } from '../shared';
+
+// 메뉴에 식자재 등록 요청하는 함수
+interface PostIngredientRegisterParams {
+  menuId: number;
+  menuIngredients: Ingredient[];
+}
+export const postIngredientRegister = async ({
+  menuId,
+  menuIngredients,
+}: PostIngredientRegisterParams) => {
+  // body 만드는 과정 :  UI에서 사용 하는 단위를 서버에서 사용하는 단위(전부 대문자)로 변환
+  const body: PostIngredientRegisterRequestDto = {
+    ingredients: menuIngredients.map((item) => ({
+      name: item.name,
+      quantity: Number(item.quantity),
+      unit:
+        convertUiUnitToServerUnit(item.unit) ??
+        (() => {
+          throw new Error('알 수 없는 단위입니다.');
+        })(),
+    })),
+  };
+  const { data } = await authorizedApi.post(
+    `/api/menus/${menuId}/ingredients`,
+    {
+      body: JSON.stringify(body),
+    },
+  );
+  return data;
+};
