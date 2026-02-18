29c01e77a3293a5e2bc3d1ca89b64f1205dd9933
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-19 02:23:12 +0900

    feat: 대시보드 지표 카드 조회 훅 추가

--- frontend/src/hooks/dashboard/index.ts
+++ frontend/src/hooks/dashboard/index.ts
@@ -2,3 +2,5 @@ export { useDashboardTabsContext } from './useDashboardTabsContext';
 export { useDashboardTabsDialog } from './useDashboardTabsDialog';
 export { useEditCard } from './useEditCard';
 export { useDashboardCardDetailQueryOption } from './useDashboardCardDetailQueryOption';
+export { useDashboardCardList } from './useDashboardCardList';
+export { useDashboardCardSubscription } from './useDashboardCardSubscription';
