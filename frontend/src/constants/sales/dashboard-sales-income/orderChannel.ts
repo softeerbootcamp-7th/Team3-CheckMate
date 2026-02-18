522488dff0a1e69a0c1993ffa8ac19ff49ee93cd
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-19 02:20:29 +0900

    refactor: orderMethod -> orderChannel로 통일

--- frontend/src/constants/sales/dashboard-sales-income/orderChannel.ts
+++ frontend/src/constants/sales/dashboard-sales-income/orderChannel.ts
@@ -1,37 +1,36 @@
 import type { GetIncomeStructureByOrderChannelResponseDto } from '@/types/sales';
 
-export const ORDER_METHOD = {
-  EXAMPLE_TOP_TYPE: '키오스크',
+import { SALES_SOURCE } from '../salesSource';
+
+const ORDER_METHOD_KEYS = Object.keys(
+  SALES_SOURCE.ORDER_METHOD,
+) as (keyof typeof SALES_SOURCE.ORDER_METHOD)[];
+
+export const ORDER_CHANNEL = {
+  EXAMPLE_TOP_TYPE: 'KIOSK',
   EXAMPLE_TOP_SHARE: 50,
   EXAMPLE_DELTA_SHARE: 4,
-  EXAMPLE_ORDER_METHOD_DATA: [
+  EXAMPLE_ORDER_CHANNEL_DATA: [
     {
-      orderChannel: 'POS',
+      orderChannel: ORDER_METHOD_KEYS[0],
       salesAmount: 2371000,
       orderCount: 26,
       share: 25,
       deltaShare: 2.4,
     },
     {
-      orderChannel: '키오스크',
+      orderChannel: ORDER_METHOD_KEYS[1],
       salesAmount: 5329000,
       orderCount: 53,
       share: 25,
       deltaShare: 4,
     },
     {
-      orderChannel: '배달앱',
+      orderChannel: ORDER_METHOD_KEYS[2],
       salesAmount: 1986000,
       orderCount: 19,
       share: 25,
       deltaShare: -5.2,
-    },
-    {
-      orderChannel: '기타',
-      salesAmount: 954000,
-      orderCount: 24,
-      share: 25,
-      deltaShare: -1.8,
     },
   ] as const satisfies GetIncomeStructureByOrderChannelResponseDto['items'],
   DOUGHNUT_CHART_TITLE: '주문수단별 매출 관련 도넛 차트',
