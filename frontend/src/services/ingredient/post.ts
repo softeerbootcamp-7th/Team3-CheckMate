f90741fdcd54031bc44a204372434337c2d6358e
minseon <mskwon0601@gmail.com>
2026-02-19 14:56:55 +0900

    chore: 식자재 등록 api 통신용 body 만드는 유틸 따로 빼고 그걸 사용

--- frontend/src/services/ingredient/post.ts
+++ frontend/src/services/ingredient/post.ts
@@ -1,6 +1,5 @@
 import type { Ingredient } from '@/types/ingredient';
-import type { PostIngredientRegisterRequestDto } from '@/types/ingredient';
-import { convertUiUnitToServerUnit } from '@/utils/ingredient/convertUnit';
+import { buildIngredientRegisterBody } from '@/utils/ingredient';
 
 import { authorizedApi } from '../shared';
 
@@ -14,17 +13,8 @@ export const postIngredientRegister = async ({
   menuIngredients,
 }: PostIngredientRegisterParams) => {
   // body 만드는 과정 :  UI에서 사용 하는 단위를 서버에서 사용하는 단위(전부 대문자)로 변환
-  const body: PostIngredientRegisterRequestDto = {
-    ingredients: menuIngredients.map((item) => ({
-      name: item.name,
-      quantity: Number(item.quantity),
-      unit:
-        convertUiUnitToServerUnit(item.unit) ??
-        (() => {
-          throw new Error('알 수 없는 단위입니다.');
-        })(),
-    })),
-  };
+  const body = buildIngredientRegisterBody({ menuIngredients });
+
   const { data } = await authorizedApi.post(
     `/api/menus/${menuId}/ingredients`,
     {
