65521a1a50ba38270b3e18a9818921d80d023f94
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-19 20:13:17 +0900

    chore: 중복된 코드 제거 및 nullable하게 변경

--- frontend/src/types/menu/dto/getPopularMenuCombinationDto.ts
+++ frontend/src/types/menu/dto/getPopularMenuCombinationDto.ts
@@ -2,28 +2,7 @@ export interface PairedMenu {
   menuName: string;
   count: number;
 }
-// PoplularMenuCombination 예시
-// {
-//       "baseMenuName": "불고기 버거",
-//       "pairedMenus": [
-//         { "menuName": "감자튀김", "count": 80 },
-//         { "menuName": "콜라", "count": 70 }
-//       ]
-//     }
-export interface PoplularMenuCombination {
-  baseMenuName: string;
-  pairedMenus: PairedMenu[];
-}
-
-export interface GetPopularMenuCombinationResponseDto {
-  items: PoplularMenuCombination[];
-}
-
-export interface PairedMenu {
-  menuName: string;
-  count: number;
-}
-// PoplularMenuCombination 예시
+// PopularMenuCombination 예시
 // {
 //       "baseMenuName": "불고기 버거",
 //       "pairedMenus": [
@@ -32,8 +11,8 @@ export interface PairedMenu {
 //       ]
 //     }
 export interface PopularMenuCombination {
-  baseMenuName: string;
-  pairedMenus: PairedMenu[];
+  baseMenuName: string | null;
+  pairedMenus: PairedMenu[] | null;
 }
 
 export interface GetPopularMenuCombinationResponseDto {
