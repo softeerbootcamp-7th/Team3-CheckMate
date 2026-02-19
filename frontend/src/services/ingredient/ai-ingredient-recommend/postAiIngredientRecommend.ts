09ef8c72e3e31f7643c25981dd2f582240649097
minseon <mskwon0601@gmail.com>
2026-02-19 18:07:12 +0900

    feat: ai 식자재 자동 생성 api 서비스 구현

--- frontend/src/services/ingredient/ai-ingredient-recommend/postAiIngredientRecommend.ts
+++ frontend/src/services/ingredient/ai-ingredient-recommend/postAiIngredientRecommend.ts
@@ -1,21 +1,26 @@
 import { authorizedApi } from '@/services/shared';
-import type {
-  PostAiIngredientRecommendRequestDto,
-  PostAiIngredientRecommendResponseDto,
-} from '@/types/ingredient';
+import type { PostAiIngredientRecommendResponseDto } from '@/types/ingredient';
+import { convertServerUnitToUiUnit } from '@/utils/ingredient';
 
+interface PostAiIngredientRecommendParams {
+  menuId: number;
+}
 // AI에서 식재료 추천 받아오는 함수
-export const postAiIngredientRecommend = async (
-  dto: PostAiIngredientRecommendRequestDto,
-) => {
+export const postAiIngredientRecommend = async ({
+  menuId,
+}: PostAiIngredientRecommendParams) => {
   // 오류 발생하면 tanstack query에서 자동으로 잡아 onError 콜백으로 넘긴다 -> try catch 불필요
   const { data } =
     await authorizedApi.post<PostAiIngredientRecommendResponseDto>(
-      '/api/ingredient/ai-ingredient-recommend',
-      {
-        body: JSON.stringify(dto),
-      },
+      `/api/menus/${menuId}/auto-complete!`,
     );
-
-  return data;
+  // 서버에서 보내주는 단위(대문자)를 UI에서 사용하는 단위로 변환
+  return {
+    menuName: data.menuName,
+    ingredients: data.ingredients.map((item) => ({
+      name: item.name,
+      quantity: item.quantity.toString(),
+      unit: convertServerUnitToUiUnit(item.unit),
+    })),
+  };
 };
