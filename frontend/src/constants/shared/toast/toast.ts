db822c60af7687cdc8afd19944025f97aa3267fb
minseon <mskwon0601@gmail.com>
2026-02-19 18:06:03 +0900

    chore: 토스트 기볼 설정 수정

--- frontend/src/constants/shared/toast/toast.ts
+++ frontend/src/constants/shared/toast/toast.ts
@@ -2,5 +2,5 @@ export const TOAST_DEFAULT = {
   DURATION: 3500, // 3.5초 동안 띄워져있음
   POSITION: 'bottom-center',
   STYLE:
-    '!bg-grey-900 !text-grey-50 !border-none !max-w-auto !w-max body-small-semibold px-400',
+    'bg-grey-900! text-grey-50! border-none! body-small-semibold px-400 max-w-none! w-max! min-w-[360px]! ',
 } as const;
