662d7f2932744a7eb5815e222f2d72d79c20b96e
lwjmcn <lwjmcn@users.noreply.github.com>
2026-02-19 05:06:02 +0000

    deploy: dcfe9c7707a354746a44d3e3c0857fff70449184

---
+++ assets/RevenueCalendar.stories-1MLjaNcY.js
@@ -0,0 +1,9 @@
+import{j as r}from"./jsx-runtime-u17CrQMm.js";import{r as n}from"./iframe-BiCE27Du.js";import{R as a}from"./RevenueCalendar-MOhRSlQX.js";import"./preload-helper-Cv0CY36J.js";import"./periods-BueKGNxC.js";import"./index-BosFzb5z.js";import"./index-GF96FoPb.js";import"./index-Dj2A59Xf.js";import"./index-A_SJPeZT.js";import"./chevron-right-rerr_Kjj.js";import"./createLucideIcon-B4M7D5CI.js";const R={title:"components/shared/calendar/RevenueCalendar",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{selectedDate:{control:"date"}}},d=t=>{const[s,o]=n.useState(t.selectedDate);return r.jsx(a,{selectedDate:s,setSelectedDate:o})},e={args:{selectedDate:void 0,setSelectedDate:()=>{}},render:t=>r.jsx(d,{...t})};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    selectedDate: undefined,
+    setSelectedDate: () => {}
+  },
+  render: args => {
+    return <DefaultRevenueCalendarStory {...args} />;
+  }
+}`,...e.parameters?.docs?.source}}};const v=["Default"];export{e as Default,v as __namedExportsOrder,R as default};
--- assets/SignInMainSection.stories-4QGbJsqN.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-u17CrQMm.js";import{S as o}from"./SignInMainSection-C0RZic1k.js";import"./FeatureCard-C_ACkN2b.js";import"./SignInButton-DhVNh3h5.js";import"./index-BtZw3gdd.js";import"./iframe-B1BLyUuq.js";import"./preload-helper-Cv0CY36J.js";import"./periods-BueKGNxC.js";import"./index-GF96FoPb.js";import"./index-DxkZsqTK.js";import"./index-DUyLIu5b.js";const{fn:s}=__STORYBOOK_MODULE_TEST__,f={title:"components/sign-in/sign-in-main-section/SignInMainSection",component:o,parameters:{layout:"fullscreen"},tags:["autodocs"]},r={decorators:[n=>e.jsx("div",{className:"h-screen w-screen",children:e.jsx(n,{})})],args:{onClick:s()},render:n=>e.jsx(o,{...n})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  decorators: [Story => <div className="h-screen w-screen">
-        <Story />
-      </div>],
-  args: {
-    onClick: fn()
-  },
-  render: args => <SignInMainSection {...args} />
-}`,...r.parameters?.docs?.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,f as default};
