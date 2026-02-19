719c37b16044bdeec5668f367873027a27898c4d
minseon <mskwon0601@gmail.com>
2026-02-19 17:21:44 +0900

    chore: ai 식자재 자동완성 목데이터 수정

--- frontend/src/mocks/ingredient/aiIngredientRecommendHandler.ts
+++ frontend/src/mocks/ingredient/aiIngredientRecommendHandler.ts
@@ -5,7 +5,7 @@ import type { SuccessResponse } from '@/services/shared';
 import type { PostAiIngredientRecommendResponseDto } from '@/types/ingredient';
 
 export const aiIngredientRecommendHandler = [
-  mswHttp.post('/api/ingredient/ai-ingredient-recommend', async () => {
+  mswHttp.post('/api/menus/:menuId/auto-complete', async () => {
     // 2초 딜레이 줘서 로딩 중(스켈레톤 UI) 보이게 -> 나중에 삭제해야함
     await new Promise((resolve) => setTimeout(resolve, 2000));
     return HttpResponse.json<
@@ -14,11 +14,28 @@ export const aiIngredientRecommendHandler = [
       {
         success: true,
         data: {
+          menuName: '오렌지 주스',
           ingredients: [
-            { ingredientId: '1', name: '딸기', amount: '200', unit: 'g' },
-            { ingredientId: '2', name: '우유', amount: '120', unit: 'ml' },
-            { ingredientId: '4', name: '연유', amount: '5', unit: 'ml' },
-            { ingredientId: '5', name: '얼음', amount: '100', unit: 'g' },
+            {
+              name: '오렌지',
+              quantity: 200,
+              unit: 'G',
+            },
+            {
+              name: '설탕',
+              quantity: 15,
+              unit: 'G',
+            },
+            {
+              name: '물',
+              quantity: 50,
+              unit: 'ML',
+            },
+            {
+              name: '레몬즙',
+              quantity: 10,
+              unit: 'ML',
+            },
           ],
         },
         message: '식재료 추천 성공',
