02d8af44ac03cd5457ac0b728c363d014a44bf42
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-20 00:58:38 +0900

    feat: 매출 추이 관련 query key 정의

--- frontend/src/services/sales/keys.ts
+++ frontend/src/services/sales/keys.ts
@@ -1,3 +1,6 @@
+import type { SalesMetricCardCodes } from '@/constants/sales';
+import type { GetAnalysisDetailQuery } from '@/types/analysis';
+
 export const salesKeys = {
   all: ['sales'] as const,
   // 매출 현황 섹션의 공통 쿼리 키
@@ -8,4 +11,44 @@ export const salesKeys = {
   source: () => [...salesKeys.all, 'source'] as const,
   // 매출 추이 섹션의 공통 쿼리 키
   trends: () => [...salesKeys.all, 'trends'] as const,
+  dailyRevenueTrend: (
+    salesMetricCardCode: SalesMetricCardCodes,
+    query: GetAnalysisDetailQuery,
+  ) =>
+    [
+      ...salesKeys.all,
+      'dailyRevenueTrend',
+      salesMetricCardCode,
+      query,
+    ] as const,
+  weeklyRevenueTrend: (
+    salesMetricCardCode: SalesMetricCardCodes,
+    query: GetAnalysisDetailQuery,
+  ) =>
+    [
+      ...salesKeys.all,
+      'weeklyRevenueTrend',
+      salesMetricCardCode,
+      query,
+    ] as const,
+  monthlyRevenueTrend: (
+    salesMetricCardCode: SalesMetricCardCodes,
+    query: GetAnalysisDetailQuery,
+  ) =>
+    [
+      ...salesKeys.all,
+      'monthlyRevenueTrend',
+      salesMetricCardCode,
+      query,
+    ] as const,
+  yearlyRevenueTrend: (
+    salesMetricCardCode: SalesMetricCardCodes,
+    query: GetAnalysisDetailQuery,
+  ) =>
+    [
+      ...salesKeys.all,
+      'yearlyRevenueTrend',
+      salesMetricCardCode,
+      query,
+    ] as const,
 };
