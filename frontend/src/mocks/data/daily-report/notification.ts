4807381eda2f32b60dfd694792133da9187d72c1
lwjmcn <clarecse02@gmail.com>
2026-02-20 18:16:39 +0900

    chore: mock 데이터에서 타임존 제거

--- frontend/src/mocks/data/daily-report/notification.ts
+++ frontend/src/mocks/data/daily-report/notification.ts
@@ -4,19 +4,19 @@ export const NOTIFICATION_DATA: Notification[] = [
   {
     notificationId: '1',
     content: '1월 14일 하루 리포트가 발행되었습니다.',
-    createdAt: '2026-02-09T05:00:00Z',
+    createdAt: '2026-02-09T05:00:00',
     isRead: false,
   },
   {
     notificationId: '2',
     content: '1월 14일 하루 리포트가 발행되었습니다.',
-    createdAt: '2026-02-07T15:00:00Z',
+    createdAt: '2026-02-07T15:00:00',
     isRead: true,
   },
   {
     notificationId: '3',
     content: '1월 14일 하루 리포트가 발행되었습니다.',
-    createdAt: '2024-05-31T16:45:00Z',
+    createdAt: '2024-05-31T16:45:00',
     isRead: false,
   },
 ];
