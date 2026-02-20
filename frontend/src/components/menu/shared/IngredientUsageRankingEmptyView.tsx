6178988750f47d2b6fa6c99353191a4deb45c601
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-21 02:53:34 +0900

    chore: 식재료 소진량 empty view 오늘 관련 문구 조건부 렌더링 방식 수정

--- frontend/src/components/menu/shared/IngredientUsageRankingEmptyView.tsx
+++ frontend/src/components/menu/shared/IngredientUsageRankingEmptyView.tsx
@@ -7,14 +7,14 @@ interface IngredientUsageRankingEmptyViewProps {
 export const IngredientUsageRankingEmptyView = ({
   isCustomPeriod = false,
 }: IngredientUsageRankingEmptyViewProps) => {
-  const date = isCustomPeriod ? '' : '오늘';
   return (
     <div className="flex h-full flex-col items-center justify-center gap-2">
       <div className="bg-brand-50 size-10 rounded-full p-2">
         <ShoppingBasket className="text-brand-main size-full" />
       </div>
       <p className="body-large-bold text-grey-900 text-center whitespace-pre">
-        {date} <span className="text-brand-main">소진된 식재료</span>가{'\n'}
+        {isCustomPeriod ? '' : '오늘 '}
+        <span className="text-brand-main">소진된 식재료</span>가{'\n'}
         아직 없어요
       </p>
       <span className="body-small-medium text-grey-700 text-center whitespace-pre">
