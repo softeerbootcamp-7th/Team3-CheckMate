7c61e9e8e3e3baa18e58da7b09992dca1b0b4269
minseon <mskwon0601@gmail.com>
2026-02-20 20:52:20 +0900

    chore: 대시보드에서 사용하고 있는 단위인 ingredientUnit 복구

--- frontend/src/constants/ingredient/index.ts
+++ frontend/src/constants/ingredient/index.ts
@@ -1,4 +1,4 @@
-export { SERVER_INGREDIENT_UNIT } from './ingredientUnit';
-export type { ServerIngredientUnit } from './ingredientUnit';
+export { SERVER_INGREDIENT_UNIT, INGREDIENT_UNIT } from './ingredientUnit';
+export type { ServerIngredientUnit, IngredientUnit } from './ingredientUnit';
 export { DEFAULT_INGREDIENT } from './defaultIngredient';
 export { ITEMS_PER_PAGE } from './ingredientManagement';
--- frontend/src/constants/ingredient/ingredientUnit.ts
+++ frontend/src/constants/ingredient/ingredientUnit.ts
@@ -1,5 +1,14 @@
 import type { ValueOf } from '@/utils/shared';
 
+export const INGREDIENT_UNIT = {
+  ml: 'ml',
+  L: 'L',
+  g: 'g',
+  kg: 'kg',
+} as const;
+
+export type IngredientUnit = ValueOf<typeof INGREDIENT_UNIT>;
+
 // 서버에서 사용하고 있는 식재료 단위
 export const SERVER_INGREDIENT_UNIT = {
   G: 'G',
