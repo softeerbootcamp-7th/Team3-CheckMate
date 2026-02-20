09ed55b9ff0d089de9fd09c10f402fc12a9275a8
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-21 01:22:09 +0900

    feat: 메뉴별 매출 랭킹 fetch 및 리스트 데이터 반환 커스텀 훅 추가

---
+++ frontend/src/hooks/menu/index.ts
@@ -0,0 +1,1 @@
+export { useMenuSalesRank } from './popular-menu';
---
+++ frontend/src/hooks/menu/popular-menu/useMenuSalesRank.ts
@@ -0,0 +1,41 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { MENU_SALES_RANK } from '@/constants/menu';
+import type { PERIOD_PRESET_KEYS, PeriodType } from '@/constants/shared';
+import { menuOptions } from '@/services/menu';
+import type { GetMenuSalesRankingResponseDto } from '@/types/menu';
+import { getMenuSalesRankCardCode } from '@/utils/menu';
+import { formatDateISO } from '@/utils/shared';
+
+interface UseMenuSalesRankProps {
+  periodType?: PeriodType<typeof PERIOD_PRESET_KEYS.today7_30>;
+  startDate?: Date;
+  endDate?: Date;
+}
+
+export const useMenuSalesRank = ({
+  periodType,
+  startDate,
+  endDate,
+}: UseMenuSalesRankProps) => {
+  const cardCode = getMenuSalesRankCardCode(periodType);
+
+  const { data } = useSuspenseQuery(
+    menuOptions.menuSalesRank<GetMenuSalesRankingResponseDto>({
+      analysisCardCode: cardCode,
+      customPeriod: !periodType,
+      from: startDate ? formatDateISO(startDate) : undefined,
+      to: endDate ? formatDateISO(endDate) : undefined,
+    }),
+  );
+
+  const displayedRankItems = data.items.slice(
+    0,
+    MENU_SALES_RANK.MAX_DISPLAYED_RANK_ITEMS,
+  );
+
+  return {
+    cardCode,
+    displayedRankItems,
+  };
+};
