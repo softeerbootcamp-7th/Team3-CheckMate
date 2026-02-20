dabe229afa0d5da4aeb0c97f9265c1b1fe34a1c6
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-21 01:19:00 +0900

    feat: 상세 분석 인기 메뉴 카테고리 periodProvider 추가

--- frontend/src/pages/menu-page/MenuPage.tsx
+++ frontend/src/pages/menu-page/MenuPage.tsx
@@ -4,6 +4,7 @@ import {
   MenuSalesPatternOverview,
   PopularMenuOverview,
 } from '@/components/menu';
+import { PopularMenuPeriodTypeProvider } from '@/components/menu/popular-menu';
 import { useMainScrollTop } from '@/hooks/shared';
 
 export const MenuPage = () => {
@@ -11,7 +12,9 @@ export const MenuPage = () => {
 
   return (
     <div className="my-32.5 flex flex-col gap-12" ref={handleMainScrollToTop}>
-      <PopularMenuOverview />
+      <PopularMenuPeriodTypeProvider>
+        <PopularMenuOverview />
+      </PopularMenuPeriodTypeProvider>
       <MenuSalesPatternOverview />
       <IngredientConsumptionOverview />
       <MenuCombinationOverview />
