b6dff9b9e06f20af2790e2915254575a6d1e17bc
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-21 18:00:53 +0900

    chore: 오늘이 아닌 경우 아직 텍스트 조건부 렌더링

--- frontend/src/components/menu/shared/IngredientUsageRankingEmptyView.tsx
+++ frontend/src/components/menu/shared/IngredientUsageRankingEmptyView.tsx
@@ -15,7 +15,7 @@ export const IngredientUsageRankingEmptyView = ({
       <p className="body-large-bold text-grey-900 text-center whitespace-pre">
         {isCustomPeriod ? '' : '오늘 '}
         <span className="text-brand-main">소진된 식재료</span>가{'\n'}
-        아직 없어요
+        {isCustomPeriod ? '' : '아직 '}없어요
       </p>
       <span className="body-small-medium text-grey-700 text-center whitespace-pre">
         첫 주문이 들어오면{'\n'}식재료 소진량을 분석해 드릴게요!
