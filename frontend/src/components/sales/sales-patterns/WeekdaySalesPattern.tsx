7a074bca0ba8e2f261b3a87688629dfba9ff8970
minseon <mskwon0601@gmail.com>
2026-02-22 03:10:09 +0900

    feat: 상세분석>메출분석>요일별 매출 카드 구현

---
+++ frontend/src/components/sales/sales-patterns/WeekdaySalesPattern.tsx
@@ -0,0 +1,68 @@
+import {
+  BarChart,
+  DefaultCardWrapper,
+  PeriodSelect,
+} from '@/components/shared';
+import { SALES_METRIC } from '@/constants/sales';
+import { PERIOD_PRESET_KEYS } from '@/constants/shared';
+import { useWeekdaySalesPattern } from '@/hooks/sales';
+
+import { useWeekdaySalesPatternPeriodType } from './period-type-provider';
+
+export const WeekdaySalesPattern = () => {
+  const {
+    periodType,
+    setPeriodType,
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate,
+  } = useWeekdaySalesPatternPeriodType();
+
+  // 오늘 요일에 해당하는 idx:  월 -> 0, ... 일 -> 6
+  const todayWeekDayIdx = (new Date().getDay() + 6) % 7;
+
+  const { label: peakTimeLabel } =
+    SALES_METRIC.SALES_PATTERN.WEEKDAY_SALES_PATTERN;
+
+  const { weekdaySalesPatternBarData, weekdaySalesPatternLabelData } =
+    useWeekdaySalesPattern({
+      periodType,
+      startDate,
+      endDate,
+    });
+
+  return (
+    <DefaultCardWrapper
+      className="flex h-57 w-full flex-col justify-start gap-5"
+      title={peakTimeLabel}
+      aria-label={peakTimeLabel}
+    >
+      <PeriodSelect
+        periodPreset={PERIOD_PRESET_KEYS.recent4W}
+        periodType={periodType}
+        setPeriodType={setPeriodType}
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+        className="absolute top-5 right-2"
+      />
+      <div>
+        <BarChart
+          viewBoxWidth={1020}
+          viewBoxHeight={150}
+          hasXAxis
+          hasBarGradient
+          xAxisType="default"
+          activeDataIndex={todayWeekDayIdx}
+          activeTooltip
+          chartTitle={`${peakTimeLabel} 차트`}
+          chartDescription={`매장이 바쁜 때를 파악해요.`}
+          primarySeries={weekdaySalesPatternBarData}
+          secondarySeries={weekdaySalesPatternLabelData}
+        />
+      </div>
+    </DefaultCardWrapper>
+  );
+};
