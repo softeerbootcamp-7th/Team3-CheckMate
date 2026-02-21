dcbbd36a71a0e9450850366a3a6dbfc1b7470848
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-21 16:41:28 +0900

    feat: 로그아웃 요청 시 버튼 비활성화 추가

--- frontend/src/components/setting/setting-auth/SignOutButton.tsx
+++ frontend/src/components/setting/setting-auth/SignOutButton.tsx
@@ -6,7 +6,7 @@ import { ROUTE_PATHS } from '@/constants/shared';
 import { postSignOut } from '@/services/auth';
 
 export const SignOutButton = () => {
-  const { mutate: signOut } = useMutation({
+  const { mutate: signOut, isPending } = useMutation({
     mutationFn: postSignOut,
     onSuccess: () => {
       window.location.replace(ROUTE_PATHS.SIGN_IN);
@@ -20,6 +20,7 @@ export const SignOutButton = () => {
     <Button
       className="rounded-400 bg-special-card-bg body-large-medium text-grey-700 flex h-fit w-full items-center justify-between p-500! hover:underline"
       onClick={() => signOut()}
+      disabled={isPending}
     >
       <span className="text-grey-700 body-large-medium! h-full">로그아웃</span>
     </Button>
