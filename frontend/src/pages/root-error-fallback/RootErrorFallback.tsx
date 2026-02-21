3cd5aa02e12fb3c21a99d468a2d50a71eacd653e
lee0jae330 <xingxing2001@cau.ac.kr>
2026-02-22 01:47:48 +0900

    feat: root error boundary 추가

--- frontend/src/pages/root-error-fallback/RootErrorFallback.tsx
+++ frontend/src/pages/root-error-fallback/RootErrorFallback.tsx
@@ -1,8 +1,11 @@
 import { Navigate, useRouteError } from 'react-router-dom';
 
+import { RefreshCcwIcon } from 'lucide-react';
+
+import { Button } from '@/components/shared/shadcn-ui';
+import { CDN_BASE_URL } from '@/constants/shared';
 import { isApiError } from '@/services/shared';
 
-// TODO: 에러 페이지 디자인 추가
 export const RootErrorFallback = () => {
   const error = useRouteError();
 
@@ -12,7 +15,31 @@ export const RootErrorFallback = () => {
         return <Navigate to="/sign-in" replace />;
     }
   }
-  const errorMessage = error instanceof Error ? error.message : 'Unknown error';
 
-  return <div>에러 발생 : {errorMessage}</div>;
+  return (
+    <div className="flex size-full flex-col items-center justify-center gap-3">
+      <object
+        data={`${CDN_BASE_URL}/assets/images/logoWithTitle.svg`}
+        className="absolute top-20 left-20"
+      />
+      <object data={`${CDN_BASE_URL}/assets/images/pos_not_connected.svg`} />
+
+      <div className="flex flex-col items-center gap-3.75">
+        <h1 className="headline-small-semibold text-grey-900">
+          서비스를 일시적으로 이용할 수 없어요
+        </h1>
+        <p className="body-large-medium text-grey-700">
+          예상치 못한 오류가 발생하여 페이지를 불러오지 못했습니다.{'\n'}잠시 후
+          다시 시도해주세요.
+        </p>
+        <Button
+          className="rounded-200 bg-brand-main text-grey-50 title-small-semibold! mt-5 flex h-14 w-55 items-center justify-center"
+          onClick={() => window.location.reload()}
+        >
+          새로고침하기
+          <RefreshCcwIcon className="size-4" />
+        </Button>
+      </div>
+    </div>
+  );
 };
