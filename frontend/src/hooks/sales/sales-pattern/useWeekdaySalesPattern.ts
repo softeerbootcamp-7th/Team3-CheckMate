75e1ba3c061e801131f1bc735031ae563a61ab92
minseon <mskwon0601@gmail.com>
2026-02-22 13:45:53 +0900

    chore: useMemo를 활용해 훅에서 반환하는 객체 메오이제이션

--- frontend/src/hooks/sales/sales-pattern/useWeekdaySalesPattern.ts
+++ frontend/src/hooks/sales/sales-pattern/useWeekdaySalesPattern.ts
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { SALES_METRIC, SALES_PATTERN_DETAIL } from '@/constants/sales';
@@ -36,32 +38,42 @@ export const useWeekdaySalesPattern = ({
   );
 
   // 바그래프에 해당하는 데이터(매출액)
-  const weekdaySalesPatternBarData = {
-    data: {
-      mainX: createChartData(data.items, (item) => item.day, X_UNIT),
-      mainY: createChartData(
-        data.items,
-        (item) => item.avgNetAmount,
-        MAIN_Y_UNIT,
-      ),
-    },
-    color: PRIMARY_COLOR,
-  };
+  const weekdaySalesPatternBarData = useMemo(
+    () => ({
+      data: {
+        mainX: createChartData(data.items, (item) => item.day, X_UNIT),
+        mainY: createChartData(
+          data.items,
+          (item) => item.avgNetAmount,
+          MAIN_Y_UNIT,
+        ),
+      },
+      color: PRIMARY_COLOR,
+    }),
+    [data.items, X_UNIT, MAIN_Y_UNIT, PRIMARY_COLOR],
+  );
   // 서브 레이블에 해당하는 데이터(주문 건수)
-  const weekdaySalesPatternLabelData = {
-    data: {
-      mainX: createChartData(data.items, (item) => item.day, ''),
-      mainY: createChartData(data.items, (item) => item.orderCount, SUB_Y_UNIT),
-    },
-    color: SECONDARY_COLOR,
-  };
+  const weekdaySalesPatternLabelData = useMemo(
+    () => ({
+      data: {
+        mainX: createChartData(data.items, (item) => item.day, ''),
+        mainY: createChartData(
+          data.items,
+          (item) => item.orderCount,
+          SUB_Y_UNIT,
+        ),
+      },
+      color: SECONDARY_COLOR,
+    }),
+    [data.items, SUB_Y_UNIT, SECONDARY_COLOR],
+  );
 
   // 툴팁에 상세 가격 화폐 단위로 보여주기 위한 함수
   const weekdaySalesPatternTooltipContent = (
     mainY: string,
     mainYUnit: string,
   ) => {
-    return `${formatPriceWithComma(Number(mainY)).split('.')[0]}${mainYUnit}`;
+    return `${formatPriceWithComma(Number(mainY)).split('.')[0]}}${mainYUnit}`;
   };
   return {
     weekdaySalesPatternBarData,
