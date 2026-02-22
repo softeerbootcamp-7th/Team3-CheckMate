6ce969456b1f78e36c455a0af42594cce5cde355
lwjmcn <clarecse02@gmail.com>
2026-02-23 02:58:07 +0900

    chore: 주석 수정

--- frontend/src/constants/shared/edit-card-wrapper/editCardWrapper.ts
+++ frontend/src/constants/shared/edit-card-wrapper/editCardWrapper.ts
@@ -3,6 +3,6 @@ export const EDIT_CARD_WRAPPER = {
   PADDING_SIZE: 12, // 패딩 박스 내 여백 사이즈(12px)
   HEADER_HEIGHT: 26, // 헤더 높이(위에 있는 버튼들 높이 : 26px)
   HEADER_MAIN_GAP: 16, // 헤더와 내용 사이 간격(16px)
-  MIN_WIDTH: 230, // 최소 너비 220px
-  MIN_HEIGHT: 155, // 최소 높이 150px
+  MIN_WIDTH: 230, // 최소 너비 230px
+  MIN_HEIGHT: 155, // 최소 높이 155px
 };
