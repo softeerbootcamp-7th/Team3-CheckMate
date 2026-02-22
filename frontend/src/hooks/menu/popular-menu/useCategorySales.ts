72af300fd7990adc9be77c54e84cdb828aa8f668
lwjmcn <clarecse02@gmail.com>
2026-02-22 17:29:49 +0900

    feat: 카테고리별 매출 API 훅 추가

---
+++ frontend/src/hooks/menu/popular-menu/useCategorySales.ts
@@ -0,0 +1,50 @@
+import { useMemo } from 'react';
+
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import type { PERIOD_PRESET_KEYS, PeriodType } from '@/constants/shared';
+import { menuOptions } from '@/services/menu';
+import type { GetCategorySalesResponseDto } from '@/types/menu';
+import type { DoughnutChartItem } from '@/types/shared';
+import { getCategorySalesCardCode } from '@/utils/menu';
+import { formatDateISO } from '@/utils/shared';
+
+interface UseCategorySalesProps {
+  periodType?: PeriodType<typeof PERIOD_PRESET_KEYS.today7_30>;
+  startDate?: Date;
+  endDate?: Date;
+}
+
+export const useCategorySales = ({
+  periodType,
+  startDate,
+  endDate,
+}: UseCategorySalesProps) => {
+  const cardCode = getCategorySalesCardCode(periodType);
+
+  const { data } = useSuspenseQuery(
+    menuOptions.categorySales<GetCategorySalesResponseDto>({
+      analysisCardCode: cardCode,
+      customPeriod: !periodType,
+      from: startDate ? formatDateISO(startDate) : undefined,
+      to: endDate ? formatDateISO(endDate) : undefined,
+    }),
+  );
+
+  const isEmptyCategorySales = data.items.length === 0;
+
+  const categorySalesChartData: DoughnutChartItem[] = useMemo(
+    () =>
+      data.items.map((item) => ({
+        label: item.category,
+        value: item.totalSalesAmount,
+      })),
+    [data.items],
+  );
+
+  return {
+    cardCode,
+    categorySalesChartData,
+    isEmptyCategorySales,
+  };
+};
