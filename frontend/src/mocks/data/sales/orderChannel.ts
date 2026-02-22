0cd156a664ce6109130b7f3b86ea540633850332
lwjmcn <clarecse02@gmail.com>
2026-02-22 17:52:56 +0900

    chore: mock 데이터 dto 변경

--- frontend/src/mocks/data/sales/orderChannel.ts
+++ frontend/src/mocks/data/sales/orderChannel.ts
@@ -3,21 +3,21 @@ import type { SalesSource } from '@/types/sales';
 
 export const ORDER_CHANNEL_DATA: SalesSource[] = [
   {
-    salesSourceType: SALES_SOURCE.ORDER_CHANNEL.POS,
-    revenue: 23710000,
-    count: 26,
-    changeRate: 4.4,
+    salesSource: SALES_SOURCE.ORDER_CHANNEL.POS,
+    salesAmount: 23710000,
+    orderCount: 26,
+    deltaShare: 4.4,
   },
   {
-    salesSourceType: SALES_SOURCE.ORDER_CHANNEL.KIOSK,
-    revenue: 1000,
-    count: 25,
-    changeRate: 6.7,
+    salesSource: SALES_SOURCE.ORDER_CHANNEL.KIOSK,
+    salesAmount: 1000,
+    orderCount: 25,
+    deltaShare: 6.7,
   },
   {
-    salesSourceType: SALES_SOURCE.ORDER_CHANNEL.DELIVERY_APP,
-    revenue: 1000,
-    count: 75,
-    changeRate: -5.2,
+    salesSource: SALES_SOURCE.ORDER_CHANNEL.DELIVERY_APP,
+    salesAmount: 1000,
+    orderCount: 75,
+    deltaShare: -5.2,
   },
 ];
--- frontend/src/mocks/data/sales/payMethod.ts
+++ frontend/src/mocks/data/sales/payMethod.ts
@@ -3,27 +3,27 @@ import type { SalesSource } from '@/types/sales';
 
 export const PAY_METHOD_DATA: SalesSource[] = [
   {
-    salesSourceType: SALES_SOURCE.PAY_METHOD.CARD,
-    revenue: 2371000,
-    count: 26,
-    changeRate: 4.4,
+    salesSource: SALES_SOURCE.PAY_METHOD.CARD,
+    salesAmount: 2371000,
+    orderCount: 26,
+    deltaShare: 4.4,
   },
   {
-    salesSourceType: SALES_SOURCE.PAY_METHOD.CASH,
-    revenue: 2567000,
-    count: 25,
-    changeRate: 6.7,
+    salesSource: SALES_SOURCE.PAY_METHOD.CASH,
+    salesAmount: 2567000,
+    orderCount: 25,
+    deltaShare: 6.7,
   },
   {
-    salesSourceType: SALES_SOURCE.PAY_METHOD.EASY_PAY,
-    revenue: 7531000,
-    count: 75,
-    changeRate: -5.2,
+    salesSource: SALES_SOURCE.PAY_METHOD.EASY_PAY,
+    salesAmount: 7531000,
+    orderCount: 75,
+    deltaShare: -5.2,
   },
   {
-    salesSourceType: SALES_SOURCE.PAY_METHOD.ETC,
-    revenue: 3894000,
-    count: 39,
-    changeRate: 8.4,
+    salesSource: SALES_SOURCE.PAY_METHOD.ETC,
+    salesAmount: 3894000,
+    orderCount: 39,
+    deltaShare: 8.4,
   },
 ];
