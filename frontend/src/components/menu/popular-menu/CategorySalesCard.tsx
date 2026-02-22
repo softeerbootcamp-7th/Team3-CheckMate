56188f32cdbdb8a4c3905a032ef13ceda8bb0766
lwjmcn <clarecse02@gmail.com>
2026-02-22 17:55:52 +0900

    feat: fetch boundary 추가

--- frontend/src/components/menu/popular-menu/CategorySalesCard.tsx
+++ frontend/src/components/menu/popular-menu/CategorySalesCard.tsx
@@ -1,15 +1,20 @@
-import { DefaultCardWrapper } from '@/components/shared';
+import {
+  DefaultCardFetchBoundary,
+  DefaultCardWrapper,
+} from '@/components/shared';
 
 import { CategorySalesChart } from './CategorySalesChart';
 
 export const CategorySalesCard = () => {
   return (
-    <DefaultCardWrapper
-      aria-label="카테고리별 매출"
-      className="flex h-68 flex-1 gap-5"
-      title="카테고리별 매출"
-    >
-      <CategorySalesChart />
-    </DefaultCardWrapper>
+    <DefaultCardFetchBoundary className="h-68 flex-1">
+      <DefaultCardWrapper
+        aria-label="카테고리별 매출"
+        className="flex h-68 flex-1 gap-5"
+        title="카테고리별 매출"
+      >
+        <CategorySalesChart />
+      </DefaultCardWrapper>
+    </DefaultCardFetchBoundary>
   );
 };
