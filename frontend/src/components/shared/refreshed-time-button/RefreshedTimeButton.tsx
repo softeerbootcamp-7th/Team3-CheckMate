61a27fae3075033fcc97dd0706be28cc813af58e
minseon <mskwon0601@gmail.com>
2026-02-24 21:38:21 +0900

    chore: 새로고침 시간, 새로고침 버튼 css 수정

--- frontend/src/components/shared/refreshed-time-button/RefreshedTimeButton.tsx
+++ frontend/src/components/shared/refreshed-time-button/RefreshedTimeButton.tsx
@@ -9,16 +9,20 @@ import { Button } from '../shadcn-ui';
 
 interface RefreshedTimeButtonProps {
   queryKey: QueryKey; // 해당 지표카드의 쿼리 키
+  className?: string;
 }
 
-export const RefreshedTimeButton = ({ queryKey }: RefreshedTimeButtonProps) => {
+export const RefreshedTimeButton = ({
+  queryKey,
+  className,
+}: RefreshedTimeButtonProps) => {
   const { lastUpdatedDate, isFetching, refresh } = useCardRefresh({
     prefixKey: queryKey,
     initLastUpdatedNow: false, // 첫 렌더에서 지금 시각을 새로고침된 시간으로 보이지 않게 설정
   });
   return (
-    <div className="flex flex-col items-end pb-px">
-      <div className="bg-grey-100 mt-5 mb-3 h-px w-full px-2" />
+    <div className={cn('flex flex-col items-end pb-px', className)}>
+      <div className="bg-grey-100 mt-5 mb-3 h-px px-2" />
       <div className="flex shrink-0 items-center gap-2">
         <span className="caption-large-medium text-grey-400">
           {lastUpdatedDate
@@ -33,7 +37,10 @@ export const RefreshedTimeButton = ({ queryKey }: RefreshedTimeButtonProps) => {
         >
           <RefreshCcw
             key={lastUpdatedDate?.toString()} // lastUpdatedDate가 변경될 때마다 아이콘이 재마운트되도록 key로 사용
-            className={cn('text-grey-400 animate-spin-once size-3 rotate-90')} // 재마운트 될 때 마다 한 번 회전하는 애니메이션 적용
+            className={cn(
+              'text-grey-400 size-3 rotate-90',
+              isFetching && 'animate-spin',
+            )}
           />
         </Button>
       </div>
